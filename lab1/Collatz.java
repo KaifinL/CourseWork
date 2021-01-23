be3aba1ea6a5f682dc2de74729a2b4fe669e785f
Kaifeng Lin <kefinlin@berkeley.edu>
2021-01-23 13:31:30 +0800

    Snap: HelloNumbers.java, GetEnvironmentVariables.java, Collatz.java, CheckLabConfig.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -20,10 +20,6 @@ public class Collatz {
         }
         return t;
 
-/** this method return the next number of Collatz Sequence
- * however, it doesn't judge anything,it just return the value of the next number.
- */
-
 
     }
 

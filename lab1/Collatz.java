c3076be88173cbf39565385ec6f43d03eba2267e
Kaifeng Lin <kefinlin@berkeley.edu>
2021-01-23 11:06:47 +0800

    Snap: HelloNumbers.java, GetEnvironmentVariables.java, Collatz.java, CheckLabConfig.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,15 +4,21 @@
 public class Collatz {
     public static void main(String[] args) {
         int n = 5;
-
-
-        System.out.print(n + " ");
-
+        while (n!=1) {
+            n = nextnumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println("1");
     }
     public static int nextnumber(int t) {
-        if (t % 2 == 0 ){
-            return (t//2)
+        while (t != 1) {
+
+            if (t % 2 == 0) {    /** this statement is to judge whether the peramator is an odd or not      */
+                return (t / 2);
+            }
+            return (3*t + 1);
         }
+        return t;
 
 /** this method return the next number of Collatz Sequence
  * however, it doesn't judge anything,it just return the value of the next number.

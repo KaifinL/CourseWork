e5e2e6e30321a038957fe1a1252d0dac67c75d35
Kaifeng Lin <kefinlin@berkeley.edu>
2021-01-26 18:45:29 +0800

    Snap: Model.java, TestMaxTileExists.java, Board.java, TestAtLeastOneMoveExists.java

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -181,10 +181,9 @@ public class Model extends Observable {
         if (emptySpaceExists(b)){
             return true;
         }
-        for (int i=0;i<3;i+=1) {
-            for (int j = 0; j < 3; j += 1) {
-                if (b.tile(i,j).value()==b.tile(i,j+1).value())
-                    { return true; }
+        for (int i=0;i<4;i+=1) {
+            for (int j = 0; j < 4; j += 1) {
+                if (b.tile(i,j).value()==b.tile(i,j+1).value()){return true;}
                 if (b.tile(i,j).value()==b.tile(i+1,j).value()){ return true;}
             }
 

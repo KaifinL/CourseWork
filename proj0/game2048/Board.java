2b8715015fa795057136518f0b7932b7584be3e2
Kaifeng Lin <kefinlin@berkeley.edu>
2021-01-26 17:07:50 +0800

    Snap: Model.java, Board.java, TestEmptySpace.java

--- proj0/game2048/Board.java
+++ proj0/game2048/Board.java
@@ -1,8 +1,11 @@
 package game2048;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Formatter;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 /**
  * @author hug
@@ -60,8 +63,7 @@ public class Board implements Iterable<Tile> {
     }
 
     /** Return the current Tile at (COL, ROW), where 0 <= ROW < size(),
-     *  0 <= COL < size(). Returns null if there is no tile there.
-     * @return*/
+     *  0 <= COL < size(). Returns null if there is no tile there. */
     public Tile tile(int col, int row) {
         return vtile(col, row, viewPerspective);
     }

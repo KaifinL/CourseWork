fb761e1a3b7e10365caa3ff5f4e072cb9cbf5fb5
Kaifeng Lin <kefinlin@berkeley.edu>
2021-01-27 13:39:06 +0800

    Snap: Model.java

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -114,7 +114,7 @@ public class Model extends Observable {
         int counter = 0;
         for (int n = 0;n<4;n+=1) {       /** n stands for every column */
             int u = 3; int s =2;
-            while (u > 0 & s > 0){
+            while (u >= 0 & s >= 0){
                 Tile y = board.tile(n,u);
                 Tile z = board.tile(n,s);
                 if (y == null ){

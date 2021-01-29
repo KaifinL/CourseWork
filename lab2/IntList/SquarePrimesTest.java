240561534409cf40721d17af4f2ef1eee746323b
Kaifeng Lin <kefinlin@berkeley.edu>
2021-01-29 16:22:28 +0800

    Snap: IntListExercises.java, SquarePrimesTest.java

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(14, 15, 16, 20, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 20 -> 18", lst.toString());
+        assertFalse(changed);
+    }
 }

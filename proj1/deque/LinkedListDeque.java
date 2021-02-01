a244ff4d31f6e0c660d9636ee6e0a32763c92dce
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-01 17:38:41 +0800

    Snap: LinkedListDeque.java

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -5,4 +5,10 @@ public class LinkedListDeque {
     }
     public void addfirst(int t){}
 
+    private class DequeNode{
+        public DequeNode ahead;
+        public int item;
+        public DequeNode next;
+        public DequeNode()
+    }
 }

33391722aef343314b283c8bd6bb33d92da11515
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-01 19:07:35 +0800

    Snap: LinkedListDeque.java

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -44,7 +44,7 @@ public class LinkedListDeque {
         }
         System.out.println(currentnode.item);
     }
-    public int removefirst(){
+    public Object removefirst(){
         if (sentinel.next ==null){
             return null;
         }

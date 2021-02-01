b3d0b95de194dd113f2d36563eef5e1ad0fcb916
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-01 18:18:11 +0800

    Snap: LinkedListDeque.java

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -2,19 +2,23 @@
 public class LinkedListDeque {
     /* the same name as well as the function in the lecture */
     public DequeNode sentinel;
+    int size;
 
 
     public LinkedListDeque(){
         sentinel = new DequeNode(null,7,null);
+        size = 0;
     }
     public LinkedListDeque(int t){
         sentinel = new DequeNode(null,7,null);
         sentinel.next = new DequeNode(null,t,null);
+        size = 0;
     }
     public void addfirst(int t){
         /* this variable is to inherit the sentinel.next's information */
         DequeNode formerfirst = sentinel.next;
         sentinel.next = new DequeNode(sentinel,t,formerfirst);
+        size ++;
     }
     public void addlast(int t){
         DequeNode currentnode = sentinel;    /* this variable to represent the current node */
@@ -22,6 +26,7 @@ public class LinkedListDeque {
             currentnode = currentnode.next;
         }
         currentnode.next = new DequeNode(currentnode,t,null);
+        size ++;
     }
     public boolean isEmpty(){
         if (sentinel.next == null){
@@ -29,7 +34,9 @@ public class LinkedListDeque {
         }
         return false;
     }
-    
+    public int size(){
+        return size;
+    }
 
     private class DequeNode{
         public DequeNode ahead;

43b6d4ca39b4057766bce7ea0147d275b9354c44
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-03 15:18:10 +0800

    Snap: LinkedListDeque.java, ArrayDeque.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,5 +1,15 @@
 package deque;
 
-public class ArrayDeque {
+import org.checkerframework.checker.units.qual.A;
+
+import java.lang.reflect.Array;
+
+public class ArrayDeque<Bl> {
+    int [] item;
+
+
+    public ArrayDeque(){
+        item = new int[8];
+    }
 
 }

e9033a151e207cecf0d9eb6e779b4c90978bd831
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-03 15:41:39 +0800

    Snap: LinkedListDeque.java, ArrayDeque.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -6,15 +6,24 @@ import java.lang.reflect.Array;
 
 public class ArrayDeque<Glorp> {
     Glorp [] item;
-    int beginIndex 
+    int beginIndex = 4;
+    int size = 0;
+
 
 
     public ArrayDeque(){
         item = (Glorp[]) new Object[8];
+        if (beginIndex == 0){
+            beginIndex = item.length-1;
+        }
     }
 
     public void addfirst(Glorp t){
-
+        item[beginIndex-1] = t;
+        beginIndex --;
+        size ++;
     }
+
+    public 
 
 }

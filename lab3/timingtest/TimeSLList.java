785f8573800ae5dd5611dd03b1109d2bd1fb09ca
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 15:41:22 +0800

    Snap: StopwatchDemo.java, TimeSLList.java, TimeAList.java, AList.java

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -32,8 +32,8 @@ public class TimeSLList {
         Ns.addLast(32000);
         Ns.addLast(64000);
         Ns.addLast(128000);
-        SLList item = new SLList();
-        
+        AList<Integer> 
+
     }
 
 }

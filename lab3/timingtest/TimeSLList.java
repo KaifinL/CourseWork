ac40e03cea39c09d983b563b419b3dcda6f0cd49
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 15:44:18 +0800

    Snap: StopwatchDemo.java, TimeSLList.java, TimeAList.java, AList.java, SLList.java

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -24,16 +25,31 @@ public class TimeSLList {
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
         AList<Integer> Ns = new AList<>();
-        Ns.addLast(1000);
-        Ns.addLast(2000);
-        Ns.addLast(4000);
-        Ns.addLast(8000);
-        Ns.addLast(16000);
-        Ns.addLast(32000);
-        Ns.addLast(64000);
-        Ns.addLast(128000);
-        AList<Integer> 
-
+        int [] helpList = new int []{1000,2000,4000,8000,16000,32000,64000,128000};
+        System.arraycopy(helpList,0,Ns,0,8);
+        AList<Integer> Ops = new AList<>();
+        int [] helpList2 = new int []{10000,10000,10000,10000,10000,10000,10000,10000};
+        System.arraycopy(helpList2,0,Ops,0,8);
+        AList<Double> times = new AList<>();
+        int i=0;int j=0;int x=0;
+        for (i=0;i<8;i++){
+            int a = Ns.get(i);
+            SLList item = new SLList();
+            for (j=0;j<a;j++){
+                item.addLast(j);
+            }
+            Stopwatch sw = new Stopwatch();
+            helper(item,10000);
+            double timeInSeconds = sw.elapsedTime();
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns,times,Ops);
+        }
+        public static void helper(SLList L,int M){
+        int x;
+        for (x=0;x<8;x++){
+            L.getLast();
+        }
+        }
     }
 
-}

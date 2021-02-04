45df1dfe6aad14c5d6ca423639303a349bb7a89b
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 23:46:29 +0800

    Snap: ArrayDeque.java, ArrayDequeTest.java

--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,5 +1,51 @@
 package deque;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdRandom;
+
+import static org.junit.Assert.assertEquals;
+
 public class ArrayDequeTest {
+    public static void main(String[] args) {
+        testThreeAddThreeRemove();
+        //randomTest();
+    }
+    public static void testThreeAddThreeRemove(){
+        ArrayDeque<Integer> input = new ArrayDeque<>();
+        input.addLast(4);
+        input.addLast(5);
+        input.addLast(6);
+        ArrayDeque<Integer> expect = new ArrayDeque<>();
+        expect.addFirst(6);
+        expect.addFirst(5);
+        expect.addFirst(4);
+
+        assertEquals(input,expect);
+
+    }
+    public static void randomTest() {
+        ArrayDeque<Integer> L = new ArrayDeque<>();
+        ArrayDeque<Integer> M = new ArrayDeque<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+            } else if (operationNumber == 1 & L.size() > 0) {
+                // getLast
+
+            } else if (operationNumber == 2 & L.size() > 0) {
+                // removeLast
+
+
+            } else if (operationNumber == 3) {
+                //size
+                int size = L.size();
+
+            }
+        }
+    }
 
 }

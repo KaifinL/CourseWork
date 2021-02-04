8eaf6d7dd4d024d97578a3793b7ac56f8c134b22
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 16:58:03 +0800

    Snap: TestBuggyAList.java, AListNoResizing.java

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,6 +2,8 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+import timingtest.AList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,6 +12,11 @@ import static org.junit.Assert.*;
 public class TestBuggyAList {
   // YOUR TESTS HERE
     public void testThreeAddThreeRemove(){
-        
+        AListNoResizing origin = new AListNoResizing();
+        origin.addLast(4);
+        origin.addLast(5);
+        origin.addLast(6);
+        origin.removeLast();
+        AListNoResizing expect = new AListNoResizing();
     }
 }

bafb7e7a021f20947296e262c774f28950bb2524
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 17:22:19 +0800

    Snap: TestBuggyAList.java, AListNoResizing.java

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -13,6 +13,7 @@ public class TestBuggyAList {
   // YOUR TESTS HERE
   public static void main(String[] args) {
       testThreeAddThreeRemove();
+      randomTest();
   }
     public static void testThreeAddThreeRemove(){
         AListNoResizing<Integer> origin = new AListNoResizing<>();
@@ -27,5 +28,22 @@ public class TestBuggyAList {
         assertEquals(origin.size(),origin2.size());
         assertEquals(origin.removeLast(),origin2.removeLast());
     }
-    public static void 
+    public static void randomTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            }
+        }
+    }
 }

d1e8e3edbef9f43803dd144c419c048c202488d0
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 22:24:55 +0800

    Snap: TestBuggyAList.java, BuggyAList.java

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -32,7 +32,7 @@ public class TestBuggyAList {
         AListNoResizing<Integer> L = new AListNoResizing<>();
         BuggyAList<Integer> M = new BuggyAList<>();
 
-        int N = 5000;
+        int N = 7000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {

b7ef4282665a7b8e9f0d5015c7598b2b71b29576
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-04 17:28:05 +0800

    Snap: TestBuggyAList.java, AListNoResizing.java

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -33,7 +33,7 @@ public class TestBuggyAList {
 
         int N = 500;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
+            int operationNumber = StdRandom.uniform(0, 3);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);

4c7d63c0f21b8972c37617c3f91ecf5e296d07cf
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-08 14:07:04 +0800

    Snap: ArrayDeque.java, AListNoResizing.java, MaxArrayDeque.java

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,5 +1,5 @@
 package deque;
 
-public class MaxArrayDeque extends ArrayDeque {
-    public MaxArrayDeque(Comparable T)
+public class MaxArrayDeque<Tone> extends ArrayDeque {
+    public MaxArrayDeque(Comparable <Tone> c)
 }

36c382679bcfab46a1a6a506ce35de03559cf7bd
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-12 21:44:36 +0800

    Snap: ArrayDeque.java, AListNoResizing.java, LinkedListDeque.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -51,6 +51,9 @@ public class ArrayDeque<Glorp> {
     }
 
     public Glorp removeFirst(){
+        if ((size < item.length / 4) && (size > 4)) {
+            resize(item.length / 4);
+        }
         Glorp [] a = (Glorp[]) new Object[item.length-1];
         Glorp returnStuff = item[0];
         System.arraycopy(item,1,a,0,item.length-1);

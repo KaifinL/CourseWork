8ea6bb3307c4b16b331e2e9aa7ec2b59ed916770
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-12 17:21:03 +0800

    Snap: HelloNumbers.java, GetEnvironmentVariables.java, Collatz.java, CheckLabConfig.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,27 +2,11 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-<<<<<<< HEAD
-    public static int nextnumber(int t) {
-        if (t % 2 == 0) {
-            return (t / 2);
-        }
-        return (3 * t + 1);
-    }
-    public static void main(String[] args) {
-        int n = 5;
-        System.out.print(n + " ");
-        while (n!=1) {
-            n = nextnumber(n);
-            System.out.print(n + " ");
-        }
-    }
-
-=======
-
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Buggy implementation of nextNumber!
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
@@ -39,6 +23,7 @@ public class Collatz {
             System.out.print(n + " ");
         }
         System.out.println();
->>>>>>> 7aa1b6fc79cb752e1ed844cd9cdd8c9c21e7f3d4
+
     }
+}
 

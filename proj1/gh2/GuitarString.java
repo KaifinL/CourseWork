c60cb3918486cb65b6a0fa3fc2c2a813a25df70a
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-15 01:13:08 +0800

    Snap: ArrayDeque.java, GuitarString.java, LinkedListDeque.java, MaxArrayDeque.java

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,11 +1,7 @@
 package gh2;
 
-// TODO: uncomment the following import once you're ready to start this portion
 import deque.ArrayDeque;
 import deque.Deque;
-import net.sf.saxon.expr.Component;
-// TODO: maybe more imports
-
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
     /** Constants. Do not change. In case you're curious, the keyword final
@@ -15,7 +11,6 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
      private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
@@ -26,10 +21,6 @@ public class GuitarString {
             buffer.addFirst(0.0);
         }
 
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
     }
 
 
@@ -40,10 +31,6 @@ public class GuitarString {
             buffer.removeLast();
             buffer.addFirst(r);
         }
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
@@ -56,15 +43,10 @@ public class GuitarString {
     public void tic() {
         double newDouble = (buffer.removeFirst()+ buffer.get(0))*0.5*DECAY;
         buffer.addLast(newDouble);
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.

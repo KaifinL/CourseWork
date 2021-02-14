53b192d218d9521b1f9f6dc0a47bccfd47d9c451
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-14 10:48:07 +0800

    Snap: ArrayDeque.java, MaxArrayDeque.java, AListNoResizing.java, MaxArrayDequeTest.java, LinkedListDequeTest.java

--- proj1/MaxArrayDequeTest.java
+++ proj1/MaxArrayDequeTest.java
@@ -1,3 +1,27 @@
+import deque.MaxArrayDeque;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Comparator;
+
 public class MaxArrayDequeTest {
-    
+    Comparator ForTest1 = new Comparator() {
+        @Override
+        public int compare(Object o1, Object o2) {
+            return (Integer) o1 - (Integer) o2;
+        }
+    };
+
+    @Test
+    public void Test1(){
+
+        MaxArrayDeque test1 = new MaxArrayDeque(ForTest1);
+        test1.items.addFirst(28);
+        test1.items.addFirst(34);
+        test1.items.addFirst(48);
+        test1.items.addFirst(3);
+        assertEquals(test1.max(),48);
+
+
+    }
 }

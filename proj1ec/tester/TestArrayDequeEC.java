328a6c4e2a87e051d189a2e85bd6d19e4208def1
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 00:34:08 +0800

    Snap: StudentArrayDeque.java, TestArrayDequeEC.java

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -10,15 +10,11 @@ public class TestArrayDequeEC {
     public void DequeTest() {
         StudentArrayDeque<Integer> task1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> forTask1 = new ArrayDequeSolution<>();
-        for (int i = 0; i < 1000; i += 1) {  /*  @StudentArrayDequeLaucher.java */
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            if (numberBetweenZeroAndOne < 0.5) {
+        for (int i = 0; i < 10000; i += 1) {  /*  @StudentArrayDequeLaucher.java */
+            //double numberBetweenZeroAndOne = StdRandom.uniform();
+            //if (numberBetweenZeroAndOne < 0.5) {
                 task1.addLast(i);
                 forTask1.addLast(i);
-            } else {
-                task1.addFirst(i);
-                forTask1.addFirst(i);
-            }
 
         }
         for (int i = 0; i < task1.size(); i++) {

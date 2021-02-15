ad7c6951ccbe884ea9e2b0a31b5f06214ca6935a
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 00:19:40 +0800

    Snap: StudentArrayDeque.java, TestArrayDequeEC.java, StudentArrayDequeLauncher.java

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,11 +1,15 @@
 package tester;
 import static org.junit.Assert.*;
+
+import jh61b.junit.In;
 import org.junit.Test;
 import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
     @Test
     void DequeTest() {
-        
+        StudentArrayDeque<Integer> task1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> forTask1 = new ArrayDequeSolution<>();
+
     }
 }

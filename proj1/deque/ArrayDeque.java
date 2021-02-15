e0a84a1b609f50e4ffb233927540bfc8a9ddfa9c
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-15 23:44:10 +0800

    Snap: MaxArrayDeque.java, ArrayDeque.java, LinkedListDeque.java, GuitarString.java, ArrayDequeTest.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,26 +1,29 @@
 package deque;
 
-public class ArrayDeque<T> {
-    T [] item;
-    int size;
+import java.util.Iterator;
 
-    public ArrayDeque(){
+public class ArrayDeque<T> implements Iterable<T>, Deque<T> {
+    private T [] item;
+    private int size;
+
+    public ArrayDeque() {
         item = (T[]) new Object[8];
         size = 0;
 
     }
 
-    public void addFirst(T t){
+    @Override
+    public void addFirst(T t) {
         if (size == item.length) {
             resize(size * 2);
         }
-        T [] a = (T[]) new Object[item.length+1];
-        System.arraycopy(item,0,a,1,size);
+        T [] a = (T[]) new Object[item.length];
+        System.arraycopy(item,0, a, 1, size);
         a[0] = t;
         item = a;
-        size ++;
+        size++;
     }
-
+    @Override
     public void addLast(T x) {
         if (size == item.length) {
             resize(size * 2);
@@ -29,69 +32,70 @@ public class ArrayDeque<T> {
         size = size + 1;
     }
 
-    public boolean isEmpty(){
-        if (size <= 0){
-            return true;
-        }
-        return false;
-    }
-
-    public int size(){
-        if (size <= 0){
+    @Override
+    public int size() {
+        if (size <= 0) {
             return 0;
         }
         return size;
     }
 
-    public void printDeque(){
-        for (int i= 0;i < size;i++){
+    @Override
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
             System.out.print(item[i] + " ");
         }
         System.out.println();
     }
 
-    public T removeFirst(){
+    @Override
+    public T removeFirst() {
         if ((size < item.length / 4) && (size > 4)) {
             resize(item.length / 4);
         }
-        T [] a = (T[]) new Object[item.length-1];
-        System.arraycopy(item,1,a,0,item.length-1);
-        if (size >0){
-            size --;
-        }
+        T [] a = (T[]) new Object[item.length];
+        T returnStuff = item[0];
+        System.arraycopy(item, 1, a, 0, item.length - 1);
+        size--;
         item = a;
-        T x = getFirst();
-        return x;
+        return returnStuff;
     }
 
-    public T removeLast(){
+    @Override
+    public T removeLast() {
         if ((size < item.length / 4) && (size > 4)) {
             resize(item.length / 4);
         }
-        T x = getLast();
-        if (size > 0){
+        T x = get(size - 1);
+        if (size > 0) {
             item[size - 1] = null;
             size--;
         }
         return x;
     }
 
-    public T get(int index){
-        if (index < 0 || index >= size){
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
             return null;
         }
         return item[index];
     }
 
-    public boolean equals(Object o){
-        if (o instanceof ArrayDeque){
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof ArrayDeque) {
             int counter = 0;
-            for (int i = 0;i<min(size,((ArrayDeque)o).size);i++){
-                if (item[i] ==((ArrayDeque)o).item[i]) {
+            for (int i = 0; i < min(size, ((ArrayDeque) o).size); i++) {
+                T compare = (T) ((ArrayDeque) o).item[i];
+                if (item[i].equals(compare)) {
                     counter++;
                 }
             }
-            if (counter == size){
+            if (size != ((ArrayDeque) o).size()) {
+                return false;
+            }
+            if (counter == size) {
                 return true;
             }
         }
@@ -99,28 +103,38 @@ public class ArrayDeque<T> {
     }
 
     private void resize(int capacity) {
-        Glorp[] a = (Glorp[])  new Object[capacity];
+        T[] a = (T[])  new Object[capacity];
         for (int i = 0; i < size; i += 1) {
             a[i] = item[i];
         }
         item = a;
     }
 
-    public static int min(int a,int b){
-        if (a <= b){
+    private static int min(int a, int b) {
+        if (a <= b) {
             return a;
         }
         return b;
     }
 
-    public Glorp getLast() {
-        if (size > 0) {
-            return item[size - 1];
-        }
-        return null;
+
+    public Iterator<T> iterator() {
+        return new ArrayDequeIterator();
     }
 
-    public Glorp getFirst(){
-        return item[0];
+    private class ArrayDequeIterator implements Iterator<T> {
+        private int inSize;
+        public ArrayDequeIterator() {
+            inSize = 0;
+        }
+        public boolean hasNext() {
+            return inSize < size();
+        }
+        public T next() {
+            T returnItem = get(inSize);
+            inSize++;
+            return returnItem;
+        }
+
     }
 }

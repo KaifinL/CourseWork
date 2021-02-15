6623c08f2e5ba21b4fb161c06725c3b34ff3de1e
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-15 11:00:46 +0800

    Snap: MaxArrayDeque.java, ArrayDeque.java, LinkedListDeque.java, GuitarString.java, ArrayDequeTest.java, TestGuitarString.java

--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -15,4 +15,19 @@ public class ArrayDequeTest {
         assertEquals(test1.get(1),"newlymade");
         assertEquals(test1.get(2),0);
     }
+
+    @Test
+    public void testRemoveFirst(){
+        ArrayDeque test2 = new ArrayDeque();
+        test2.removeFirst();
+        test2.addFirst(0);
+        test2.addFirst(7);
+        test2.addFirst(9);
+        int a = (int) test2.removeFirst();
+        int b = (int) test2.removeFirst();
+        int c = (int) test2.removeFirst();
+        assertEquals(a,9);
+        assertEquals(b,7);
+        assertEquals(c,0);
+    }
 }

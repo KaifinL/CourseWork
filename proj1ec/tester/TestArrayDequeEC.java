1d02740bf9db4d8c5f0905f7c6877811f3f7bef6
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 00:21:19 +0800

    Snap: StudentArrayDeque.java, TestArrayDequeEC.java, StudentArrayDequeLauncher.java

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,6 +1,7 @@
 package tester;
 import static org.junit.Assert.*;
 
+import edu.princeton.cs.introcs.StdRandom;
 import jh61b.junit.In;
 import org.junit.Test;
 import student.StudentArrayDeque;
@@ -10,6 +11,17 @@ public class TestArrayDequeEC {
     void DequeTest() {
         StudentArrayDeque<Integer> task1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> forTask1 = new ArrayDequeSolution<>();
+        for (int i = 0; i < 10; i += 1) {  /*  @StudentArrayDequeLaucher.java */
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne < 0.5) {
+                task1.addLast(i);
+                forTask1.addLast(i);
+            } else {
+                task1.addFirst(i);
+                forTask1.addFirst(i);
+            }
+            
+        }
 
     }
 }

16e33288938f3d199d0cbfbf13e42fe1cba13023
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-15 23:46:34 +0800

    Snap: MaxArrayDeque.java, ArrayDeque.java, LinkedListDeque.java, GuitarString.java, ArrayDequeTest.java

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -16,7 +16,7 @@ public class ArrayDeque<T> implements Iterable<T>, Deque<T> {
         if (size == item.length) {
             resize(size * 2);
         }
-        T [] a = (T[]) new Object[item.length];
+        T [] a = (T[]) new Object[item.length + 1];
         System.arraycopy(item,0, a, 1, size);
         a[0] = t;
         item = a;

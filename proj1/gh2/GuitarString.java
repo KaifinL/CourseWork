11ac565dbb599704768028613acf7f667c024dea
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 13:49:15 +0800

    Snap: ArrayDeque.java, GuitarString.java, ArrayDequeTest.java, TestGuitarString.java

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -2,6 +2,8 @@ package gh2;
 
 import deque.ArrayDeque;
 import deque.Deque;
+import deque.LinkedListDeque;
+
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
     /** Constants. Do not change. In case you're curious, the keyword final
@@ -15,7 +17,7 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        buffer = new ArrayDeque<>();
+        buffer = new LinkedListDeque<>();
         int capacity = (int) Math.round(SR / frequency);
         for (int i = 0; i < capacity; i++) {
             buffer.addFirst(0.0);

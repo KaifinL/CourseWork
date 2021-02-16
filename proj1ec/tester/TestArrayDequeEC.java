8856cb4603db81ab5ddfc29f561fabc45cbf5f25
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 20:29:32 +0800

    Snap: TestArrayDequeEC.java, ArrayDequeSolution.java, StudentArrayDeque.java, AssertEqualsStringDemo.java

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -18,18 +18,20 @@ public class TestArrayDequeEC {
                 Integer a = task1.removeLast();
                 Integer b = forTask1.removeLast();
                 returnList.append("removeLast()\n");
-                assertEquals("" , a, b);
+                assertEquals(returnList.toString() , a, b);
             } else if (numberBetweenZeroAndOne < 0.5) {
                 task1.addFirst(i);
                 forTask1.addFirst(i);
+                returnList.append("addFirst(" + i + ")");
             } else if (numberBetweenZeroAndOne < 0.75) {
                 task1.addLast(i);
                 forTask1.addLast(i);
+                returnList.append("addLast(" + i + ")");
             } else if (numberBetweenZeroAndOne < 1 && task1.size() > 0) {
                 Integer a = task1.removeFirst();
                 Integer b = forTask1.removeFirst();
-                System.out.println("removeFirst()");
-                assertEquals("removeFirst()", a, b);
+                returnList.append("removeFirst()");
+                assertEquals(returnList.toString(), a, b);
             }
         }
     }

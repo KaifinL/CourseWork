aac2c4bbe6845cc7cdd763c1cc96dee845bdef4c
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 19:49:13 +0800

    Snap: TestArrayDequeEC.java, ArrayDequeSolution.java, StudentArrayDeque.java, AssertEqualsStringDemo.java

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -9,23 +9,20 @@ public class TestArrayDequeEC {
     public void DequeTest() {
         StudentArrayDeque<Integer> task1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> forTask1 = new ArrayDequeSolution<>();
-        
+
 
         for (int i = 0; i < 500; i += 1) {  /*  @StudentArrayDequeLaucher.java */
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.25 && task1.size() > 0) {
                 Integer a = task1.removeLast();
                 Integer b = forTask1.removeLast();
-                System.out.println("removeLast()" );
                 assertEquals("removeLast()", a, b);
             } else if (numberBetweenZeroAndOne < 0.5) {
                 task1.addFirst(i);
                 forTask1.addFirst(i);
-                System.out.println(("addFirst(" + i + ")"));
             } else if (numberBetweenZeroAndOne < 0.75) {
                 task1.addLast(i);
                 forTask1.addLast(i);
-                System.out.println("addLast(" + i + ")");
             } else if (numberBetweenZeroAndOne < 1 && task1.size() > 0) {
                 Integer a = task1.removeFirst();
                 Integer b = forTask1.removeFirst();

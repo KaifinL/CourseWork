f08a1e0f5fed2993a59029c65a104946809820a6
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-16 09:24:33 +0800

    Snap: TestArrayDequeEC.java, AssertEqualsStringDemo.java, StudentArrayDeque.java

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -10,6 +10,19 @@ public class TestArrayDequeEC {
     public void DequeTest() {
         StudentArrayDeque<Integer> task1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> forTask1 = new ArrayDequeSolution<>();
+
+        for (int i = 0; i < 100; i += 1) {  /*  @StudentArrayDequeLaucher.java */
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne < 0.9 && task1.size() > 0) {
+                Integer a = task1.removeFirst();
+                Integer b = forTask1.removeFirst();
+                assertEquals(a,b);
+            } else {
+                task1.addFirst(i);
+                forTask1.addFirst(i);
+            }
+
+        }
         for (int i = 0; i < 100; i += 1) {  /*  @StudentArrayDequeLaucher.java */
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.5 && task1.size() > 0) {

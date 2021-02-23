31669deb8f7010c67f6d20aaacf77f7b478c6e0d
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-23 18:46:14 +0800

    Snap: Main.java, CapersRepository.java, Dog.java, Utils.java

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -20,7 +20,7 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = Utils.join(".capers");
+    static final File CAPERS_FOLDER = Utils.join(CWD, "capers");
 
     /**
      * Does required filesystem operations to allow for persistence.

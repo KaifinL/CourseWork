f0c3810045325ed0f18631645e5e373321b669a9
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-23 15:31:14 +0800

    Snap: Main.java, CapersRepository.java, Dog.java, Utils.java

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -60,7 +60,7 @@ public class Dog implements Serializable { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        File newDog = Utils.join(DOG_FOLDER, this.name + "txt");
+        File newDog = Utils.join(DOG_FOLDER, this.name + ".txt");
         try {
             newDog.createNewFile();
         } catch (IOException e) {

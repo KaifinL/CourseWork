c1b9bc45940702ed5a35f8c4a9d5e88a7875f7cb
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-28 17:43:01 +0800

    Snap: BSTMap.java, Map61B.java, ULLMap.java, TestBSTMap.java

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -70,8 +70,7 @@ public class BSTMap<Key extends Comparable<Key>,V> implements Map61B<Key,V> {
 
     private void put(Key key, V value, BSTNode cur) {
         if (cur == null) {
-            cur.key = key;
-            cur.value = value;
+            cur = new BSTNode(key, value, null, null);
         }else {
             if (key.compareTo((Key) cur.key) > 0) {
                 put(key, value, cur.right);

7ba8f5ee5f41b4533fae497a8285565087d94f92
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-28 17:27:50 +0800

    Snap: BSTMap.java, Map61B.java, ULLMap.java

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<Key extends Comparable<Key>,V> implements Map61B<Key,V> {
-    BST KeFinTree;
+    BSTNode KeFinTree;
     int size;
 
     private void BSTMap() {
@@ -13,15 +13,37 @@ public class BSTMap<Key extends Comparable<Key>,V> implements Map61B<Key,V> {
 
     public void clear() {
         size = 0;
-        KeFinTree = new BST();
+        KeFinTree = new BSTNode();
     }
 
     public boolean containsKey(Key key) {
-        return KeFinTree.contains((Comparable) key);
+        BSTNode cur = KeFinTree;
+        while (cur != null) {
+            if (key.equals(cur.key)) {
+                return true;
+            }
+            if (key.compareTo((Key) cur.key) > 0) {
+                cur = cur.right;
+            }else {
+                cur = cur.left;
+            }
+        }
+        return false;
     }
 
     public V get(Key key) {
-        return get(key);
+        BSTNode cur = KeFinTree;
+        while (cur != null) {
+            if (key.equals(cur.key)) {
+                return (V) cur.value;
+            }
+            if (key.compareTo((Key) cur.key) > 0) {
+                cur = cur.right;
+            }else {
+                cur = cur.left;
+            }
+        }
+        return null;
     }
 
     public int size() {
@@ -29,8 +51,12 @@ public class BSTMap<Key extends Comparable<Key>,V> implements Map61B<Key,V> {
     }
 
     public void put(Key key, V value) {
-        
+        put(key, value, KeFinTree);
         size += 1;
+    }
+
+    private void put(Key key, V value, BSTNode cur) {
+        
     }
 
     public Set<Key> keySet() {
@@ -50,7 +76,7 @@ public class BSTMap<Key extends Comparable<Key>,V> implements Map61B<Key,V> {
     }
 
     private class BSTMapIterator implements Iterator<Key> {
-        private BST cur;
+        private BSTNode cur;
 
         BSTMapIterator() {
             cur = KeFinTree;
@@ -68,86 +94,18 @@ public class BSTMap<Key extends Comparable<Key>,V> implements Map61B<Key,V> {
         }
 
     }
-
-    public class BST<Key extends Comparable<Key>, Value> {
-        private Node root;             // root of BST
-
-        private class Node {
-            private Key key;           // sorted by key
-            private Value val;         // associated data
-            private Node left, right;  // left and right subtrees
-            private int size;          // number of nodes in subtree
-
-            public Node(Key key, Value val, int size) {
-                this.key = key;
-                this.val = val;
-                this.size = size;
-            }
+    private class BSTNode<Key extends Comparable<Key>, Value> {
+        private Key key;
+        private Value value;
+        private BSTNode left;
+        private BSTNode right;
+        public BSTNode() {}
+        public BSTNode(Key key, Value value, BSTNode left, BSTNode right) {
+            this.key = key;
+            this.value = value;
+            this.left = left;
+            this.right = right;
         }
-
-        /**
-         * Initializes an empty symbol table.
-         */
-        public BST() {
-        }
-
-        /**
-         * Returns true if this symbol table is empty.
-         *
-         * @return {@code true} if this symbol table is empty; {@code false} otherwise
-         */
-        public boolean isEmpty() {
-            return size() == 0;
-        }
-
-        /**
-         * Returns the number of key-value pairs in this symbol table.
-         *
-         * @return the number of key-value pairs in this symbol table
-         */
-        public int size() {
-            return size(root);
-        }
-
-        // return number of key-value pairs in BST rooted at x
-        private int size(Node x) {
-            if (x == null) return 0;
-            else return x.size;
-        }
-
-        /**
-         * Does this symbol table contain the given key?
-         *
-         * @param key the key
-         * @return {@code true} if this symbol table contains {@code key} and
-         * {@code false} otherwise
-         * @throws IllegalArgumentException if {@code key} is {@code null}
-         */
-        public boolean contains(Key key) {
-            if (key == null) throw new IllegalArgumentException("argument to contains() is null");
-            return get(key) != null;
-        }
-
-        /**
-         * Returns the value associated with the given key.
-         *
-         * @param key the key
-         * @return the value associated with the given key if the key is in the symbol table
-         * and {@code null} if the key is not in the symbol table
-         * @throws IllegalArgumentException if {@code key} is {@code null}
-         */
-        public Value get(Key key) {
-            return get(root, key);
-        }
-
-        private Value get(Node x, Key key) {
-            if (key == null) throw new IllegalArgumentException("calls get() with a null key");
-            if (x == null) return null;
-            int cmp = key.compareTo(x.key);
-            if (cmp < 0) return get(x.left, key);
-            else if (cmp > 0) return get(x.right, key);
-            else return x.val;
-        }
-
     }
+
 }

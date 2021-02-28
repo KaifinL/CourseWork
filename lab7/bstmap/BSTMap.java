27d7b39e1a0c6836dddf06e06717d7b4cd0cb12f
Kaifeng Lin <kefinlin@berkeley.edu>
2021-02-28 15:19:45 +0800

    Snap: BSTMap.java, Map61B.java

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,5 +1,37 @@
 package bstmap;
 
-public class BSTMap<K,V> implements Map61B<K,V>{
-    
+import java.util.Set;
+
+public class BSTMap<K,V> implements Map61B<K,V> {
+    public void clear() {
+
+    }
+
+    public boolean containsKey(K key) {
+        return true;
+    }
+
+    public V get(K key) {
+
+    }
+
+    public int size() {
+        return 1;
+    }
+
+    public void put(K key, V value) {
+
+    }
+
+    public Set<K> keySet() {
+
+    }
+
+    public V remove(K key) {
+
+    }
+
+    public V remove(K key, V value) {
+
+    }
 }

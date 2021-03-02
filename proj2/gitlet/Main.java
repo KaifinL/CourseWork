d1d1e33f0f583506951b62ed04a6557288a79ea3
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-02 11:47:31 +0800

    Snap: Main.java, Repository.java, GitletException.java, Utils.java, Commit.java

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -23,6 +23,7 @@ public class Main {
                 }else {
                     Commit initialCommit = new Commit();
                     initialCommit.commitMessage("initial commit");
+                    initialCommit.setTimestamp();
 
                 }
                 // TODO: handle the `init` command

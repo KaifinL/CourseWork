a70fe9ff31fa39aa51e4485f97797a61a0a05020
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-04 17:50:19 +0800

    Snap: Main.java, Commit.java, Repository.java, gitlet-design.md, StagingArea.java, Blobs.java

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,7 @@
 package gitlet;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import static gitlet.Utils.*;
 
@@ -82,6 +83,8 @@ public class Repository {
         while(curr != null) {
             System.out.println("===");
             System.out.println("commit " + curr.id);
+            SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+            System.out.println(formatter.format(curr.getTimestamp()));
         }
     }
 

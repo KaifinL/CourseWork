6a9d2b2b18bdd9c1bf2af3201cefc377b71c388c
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-04 08:40:51 +0800

    Snap: Main.java, Commit.java, Utils.java, Repository.java, Blobs.java

--- proj2/gitlet/Blobs.java
+++ proj2/gitlet/Blobs.java
@@ -1,5 +1,7 @@
 package gitlet;
 
+import java.io.File;
+
 /**
  * this class is to represent the changed contents of files and to make things easier
  * also by this class we are able to keep track of what files have been changed so we are able to store
@@ -8,9 +10,13 @@ package gitlet;
  */
 public class Blobs {
 
+    private String BlobContent;
+    private String BlobsName;
     /**
      * TODO: some variables like blob itself should be created
      * TODO: how to represent a file's content by an blob object?I suppose 'readObject' will be useful.
      */
-    
+    public Blobs(File tobeRead) {
+        this.BlobContent = Utils.readContentsAsString(tobeRead);
+    }
 }

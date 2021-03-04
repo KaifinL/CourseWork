2f05ed7ab7824a5d40463f88ec5ac0fafe3433b3
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-04 08:53:58 +0800

    Snap: Main.java, Commit.java, Utils.java, Repository.java, Blobs.java, StagingArea.java

--- proj2/gitlet/StagingArea.java
+++ proj2/gitlet/StagingArea.java
@@ -1,2 +1,20 @@
-package gitlet;public class StagingArea {
+package gitlet;
+
+import java.util.HashMap;
+
+/** this class is basically for adding and removing things, just to make things easier. */
+public class StagingArea {
+
+    /** some useful variables are listed here
+     *
+     */
+
+    public static HashMap<String, String> addition;
+    public static HashMap<String, String> removal;
+
+
+    /**We should now list the methods which will be helpful storing information of files.
+     * the contents needed to be stored are file's name,file's content
+     * TODO: make the 'addition variable store the adding information including name and content'
+     */
 }

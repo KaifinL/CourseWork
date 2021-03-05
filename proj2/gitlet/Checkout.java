0b6e532c6e14a918f5fcdd7a99628b63408d5ec6
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-05 17:19:11 +0800

    Snap: Main.java, Commit.java, Repository.java, StagingArea.java, Checkout.java

--- proj2/gitlet/Checkout.java
+++ proj2/gitlet/Checkout.java
@@ -5,6 +5,11 @@ import java.io.File;
 public class Checkout {
 
     public static void checkout1(String a) {
-        File tobeCheckedOut = Utils.join(Repository.CWD)
+        /**
+         * Takes the version of the file as it exists in the head commit and puts it
+         * in the working directory, overwriting the version of the file that’s already there
+         * if there is one. The new version of the file is not staged.
+         */
+        File Head = Utils.join(Repository.Commits, Repository.Head.getId());
     }
 }

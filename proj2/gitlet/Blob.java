dcc6e8d3d675f2db0e22e3ac9703dc6beb5ec3b4
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-06 16:45:45 +0800

    Snap: Main.java, Commit.java, Repository.java, StagingArea.java, gitlet-design.md, Utils.java, Blob.java

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -9,6 +9,7 @@ import java.io.File;
 public class Blob {
 
     private byte[] BlobContent;
+    private String blobId;
     /**
      * TODO: some variables like blob itself should be created
      * TODO: how to represent a file's content by an blob object?I suppose 'readObject' will be useful.
@@ -17,11 +18,16 @@ public class Blob {
     // create a blob by the pass-in file
     public Blob(File tobeRead) {
         this.BlobContent = Utils.readContents(tobeRead);
+        this.blobId = Utils.sha1(this);
     }
 
 
     // return the content of a Blob
     public byte[] getBlob() {
         return this.BlobContent;
+    }
+
+    public String getBlobId() {
+        return this.blobId;
     }
 }

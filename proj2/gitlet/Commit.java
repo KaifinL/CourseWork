463429d48dc84fdaa5b2f710df6f3beef460d799
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-06 17:56:43 +0800

    Snap: Main.java, Commit.java, Repository.java, StagingArea.java, gitlet-design.md, Utils.java, Blob.java

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -44,6 +44,7 @@ public class Commit implements Serializable {
         this.snapshot = null;
         this.parentId = null;
         this.parent2Id = null;
+        this.id = Utils.sha1(this);
     }
 
     /**

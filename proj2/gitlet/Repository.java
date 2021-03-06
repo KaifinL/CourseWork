d2782b0fea6e851b08bb6eb102cc025769b7b601
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-06 19:32:52 +0800

    Snap: Main.java, Commit.java, Repository.java, StagingArea.java, gitlet-design.md, Utils.java, Blob.java

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -44,7 +44,6 @@ public class Repository {
         writeObject(InitialCommitFile, initialCommit);
         master = initialCommit;
         master = Head;
-        StagingArea.setup();
     }
 
     public static void add(String fileName) {

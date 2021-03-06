d8cca2ce5910ea638ab9d9b4839213daa2c3ee9e
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-06 19:30:20 +0800

    Snap: Main.java, Commit.java, Repository.java, StagingArea.java, gitlet-design.md, Utils.java, Blob.java

--- proj2/gitlet/StagingArea.java
+++ proj2/gitlet/StagingArea.java
@@ -13,10 +13,6 @@ public class StagingArea {
     public static HashMap<String, Blob> addition;
     public static HashMap<String, Blob> removal;
 
-    public static void setup() {
-        addition.put(null, null);
-        removal.put(null, null);
-    }
 
 
     /**We should now list the methods which will be helpful storing information of files.

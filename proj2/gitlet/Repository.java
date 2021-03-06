6458218057e96b3d0baa05eda827d9b2f0e4acf6
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-06 10:27:04 +0800

    Snap: Main.java, Commit.java, Repository.java, StagingArea.java, gitlet-design.md, Utils.java

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -126,16 +126,19 @@ public class Repository {
         }
     }
 
-    public static void find(String message) {
+    public static boolean find(String message) {
+        boolean exist = false;
         if (Utils.plainFilenamesIn(Commits) != null) {
             for (String name : Utils.plainFilenamesIn(Commits)) {
                 File currFIle = Utils.join(Commits, name);
                 Commit curr = Utils.readObject(currFIle, Commit.class);
                 if (curr.getMessage().equals(message)) {
                     System.out.println(curr.getId());
+                    exist = true;
                 }
             }
         }
+        return exist;
     }
 
 }

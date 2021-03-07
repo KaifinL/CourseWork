9a30a82c127ffeb3f1e8349b1cfbb94330b4bdfa
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-07 16:56:13 +0800

    Snap: Main.java, Utils.java, Blob.java, Commit.java, Repository.java, StagingArea.java

--- proj2/gitlet/StagingArea.java
+++ proj2/gitlet/StagingArea.java
@@ -1,33 +1,22 @@
 package gitlet;
 
+import java.io.File;
 import java.util.HashMap;
 
 /** this class is basically for adding and removing things, just to make things easier. */
 public class StagingArea {
 
-    /** some useful variables are listed here
-     *
+    /**
+     * some useful variables are listed here
      */
 
-    public static HashMap<String, String> addition = new HashMap<>();
-    public static HashMap<String, String> removal = new HashMap<>();
+    public static final File StagingArea = Utils.join(Repository.GITLET_DIR, "StagingArea");
+    public static final File addition = Utils.join(StagingArea, "addition");
+    public static final File removal = Utils.join(StagingArea, "removal");
 
 
-
-    /**We should now list the methods which will be helpful storing information of files.
+    /**
+     * We should now list the methods which will be helpful storing information of files.
      * the contents needed to be stored are file's name,file's content
      */
-
-    public static boolean containsName(HashMap a, String b) {
-        if (a == null) {
-            return false;
-        }
-        return a.containsKey(b);
-    }
-
-    public static boolean sameContents(String key, String a) {
-        return a.equals(addition.get(key));
-    }
-
-
 }

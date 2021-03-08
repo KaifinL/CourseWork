e61cd55a16d28194036a0bb9e26875ae0f86765e
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-08 18:54:09 +0800

    Snap: Map61B.java, MyHashMap.java, TestMyHashMap.java

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -167,7 +167,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        double division = size / initialSize;
+        double division = ((double)size / initialSize);
         if (division > loadFactor) {
             resize(initialSize * 2);
         }

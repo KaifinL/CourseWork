edb26cf73af1a54b5c63dcee0d256051d8277da8
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-08 20:50:11 +0800

    Snap: Main.java, Utils.java, Blob.java, Commit.java, Repository.java, StagingArea.java

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -110,9 +110,7 @@ public class Commit implements Serializable {
             }
         }
         for (String fileName : Utils.plainFilenamesIn(StagingArea.addition)) { // put all the files
-            File targetFile = Utils.join(StagingArea.addition, fileName); // in addition to snapshot
-            Blob newBlob = new Blob(targetFile); // this is the problem the blob we now create is now the same as
-            String blobId = newBlob.getBlobId(); // in the staging area.
+            String blobId = Repository.corresponding.get(fileName);
             snapshot.put(fileName, blobId);
         }
         helpDelete(StagingArea.addition); // clean all the files in the staging area.

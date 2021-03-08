addc459df77707a6886cef7fc323bd5175161cd2
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-08 22:32:30 +0800

    Snap: Main.java, Utils.java, Blob.java, Commit.java, Repository.java, StagingArea.java

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -2,13 +2,14 @@ package gitlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.HashMap;
 
 /**
  * the Blob class can somehow represent the content of a file while simply use a file can't.
  * I attempt to use it instead of direct file in the Commit 's property 'snapshot'
  */
-public class Blob {
+public class Blob implements Serializable {
 
     private byte[] BlobContent;
     private String blobId;

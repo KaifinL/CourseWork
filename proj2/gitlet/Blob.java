2bbdae51924d35289a4fa0871e310398c988d630
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-09 16:42:47 +0800

    Snap: Main.java, Utils.java, Blob.java, Commit.java, Repository.java, StagingArea.java

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -23,7 +23,7 @@ public class Blob implements Serializable {
     // create a blob by the pass-in file
     public Blob(File tobeRead) {
         this.BlobContent = Utils.readContents(tobeRead);
-
+        this.blobId = Utils.sha1(BlobContent);
         File aBlob = Utils.join(blobs, this.blobId);
         try {
             aBlob.createNewFile();

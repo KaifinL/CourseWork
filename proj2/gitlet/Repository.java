b250ca909cafe64fc896aa3c1d46f6cc99a08cb9
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-23 15:51:53 +0800

    Snap: Main.java, Utils.java, Blob.java, Commit.java, Repository.java, Branch.java, StagingArea.java

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -211,17 +211,7 @@ public class Repository {
         }else {
             String targetName = args[1];
             Commit curr = Branch.branches.get(targetName);
-            for (String FileName : Utils.plainFilenamesIn(CWD)) {
-                Commit head = Utils.readObject(HEAD, Commit.class);
-                if (!head.snapshot.containsKey(FileName) && curr.snapshot.containsKey(FileName)) {
-                    File untracked = Utils.join(CWD, FileName);
-                    Blob newOne = new Blob(untracked);
-                    String compared = curr.snapshot.get(FileName);
-                    if (newOne.getBlobId().equals(compared)) {
-                        Utils.exitWithError("There is an untracked file in the way; delete it, or add and commit it first.");
-                    }
-                }
-            }
+            checkoutFailure(curr);
             for (String fileName : curr.snapshot.keySet()) {
                 String BlobId = curr.snapshot.get(fileName);
                 File targetBlob = Utils.join(Blobs, BlobId);
@@ -343,6 +333,20 @@ public class Repository {
         }
         if (error) {
             Utils.exitWithError("A branch with that name does not exist.");
+        }
+    }
+
+    public static void checkoutFailure(Commit curr) {
+        for (String FileName : Utils.plainFilenamesIn(CWD)) {
+            Commit head = Utils.readObject(HEAD, Commit.class);
+            if (!head.snapshot.containsKey(FileName) && curr.snapshot.containsKey(FileName)) {
+                File untracked = Utils.join(CWD, FileName);
+                Blob newOne = new Blob(untracked);
+                String compared = curr.snapshot.get(FileName);
+                if (newOne.getBlobId().equals(compared)) {
+                    Utils.exitWithError("There is an untracked file in the way; delete it, or add and commit it first.");
+                }
+            }
         }
     }
 

6b5114b5e72663486cde8037eba1615a8f253246
Kaifeng Lin <kefinlin@berkeley.edu>
2021-03-28 10:16:27 +0800

    Snap: Main.java, Utils.java, Blob.java, Commit.java, Repository.java, StagingArea.java, Branch.java

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -13,7 +13,7 @@ public class Main {
      */
     public static void main(String[] args) {
         if (args.length == 0) {
-            Utils.exitWithError("Must have at least one argument.");
+            Utils.exitWithError("Please enter a command.");
         }
         String firstArg = args[0];
         switch(firstArg) {

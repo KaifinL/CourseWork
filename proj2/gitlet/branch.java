75daea910e182ef24e1c2d7a1a9458151d1b1dca
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-02 16:12:42 +0800

    branch

--- proj2/gitlet/branch.java
+++ proj2/gitlet/branch.java
@@ -1,13 +1,42 @@
 package gitlet;
 
-public class branch {
+import java.io.Serializable;
+import java.util.TreeMap;
 
-    public String name;
-    public Commit pointer;
+public class Branch implements Serializable {
 
-    public branch(String name, Commit pointer) {
+    //variable
+    private String name; // to indicates the branch name this is a instance variable.
+    private Commit currentCommit; // the current commit that this branch points to.
+    private static TreeMap<String, Commit> branches = new TreeMap<>();
+
+    //main methods
+    public Branch(String name, Commit currentCommit) {
         this.name = name;
-        this.pointer = pointer;
+        this.currentCommit = currentCommit;
+        branches.put(name, currentCommit);
     }
+
+    public void setCurrentCommit(Commit headCommit) {
+        this.currentCommit = headCommit;
+    }
+
+    public Commit getCurrentCommit() {
+        return currentCommit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static TreeMap<String, Commit> getBranches() {
+        return branches;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //helper function
 
 }

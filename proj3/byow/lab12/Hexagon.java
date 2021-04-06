840d11b971691b879d404e1c807e9601550b6245
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-06 18:23:23 +0800

    Snap: Main.java, Tileset.java, RandomWorldDemo.java, HexWorld.java, Hexagon.java, BoringWorldDemo.java

--- proj3/byow/lab12/Hexagon.java
+++ proj3/byow/lab12/Hexagon.java
@@ -1,2 +1,51 @@
-package byow.lab12;public class Hexagon {
+package byow.lab12;
+
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+public class Hexagon {
+    // variable here
+    int sideLength;
+    TETile pattern;
+
+
+    public Hexagon(int sideLength, TETile pattern) {
+        this.sideLength = sideLength;
+        this.pattern = pattern;
+    }
+
+    public void drawIt() {
+        int maximum = 3 * sideLength - 2;
+        // draw the top half hexagon
+        for (int i = sideLength; i < maximum + 1; i += 2) {
+            printLine(i, maximum, pattern);
+        }
+        // draw the bottom half hexagon
+        for (int i = 3 * sideLength - 2; i > maximum + 1; i -= 2) {
+            printLine(i, maximum, pattern);
+        }
+    }
+
+    /**
+     *
+     * @param p the specified pattern number
+     * @param q the maximum number of patterns in one row of the hexagon
+     * @param pattern the parameter that is set when creating this hexagon
+     */
+    private static void printLine(int p, int q, TETile pattern) {
+        int spaceNumber = (p - q) / 2;
+        for (int i = 0; i < spaceNumber; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < p; i++) {
+            System.out.print(pattern);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Hexagon test = new Hexagon(4, Tileset.FLOWER);
+        test.drawIt();
+    }
+
 }

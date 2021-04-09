4ddfea0bd519cb0b42c502247416b22694a0dda3
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-10 07:48:34 +0800

    Snap: RandomWorldDemo.java, HexWorld.java, BoringWorldDemo.java, Hallway.java

--- proj3/byow/Core/Hallway.java
+++ proj3/byow/Core/Hallway.java
@@ -1,6 +1,8 @@
 package byow.Core;
 
+import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
 
 // this class is basically to build the hallway by the given parameter
 public class Hallway {
@@ -8,6 +10,7 @@ public class Hallway {
     int length;
     int width;
     TETile pattern;
+    TETile[][] realHallway;
 
     // methods here
     public Hallway(int length, int width, TETile pattern) {
@@ -18,6 +21,17 @@ public class Hallway {
 
     public void drawHallway() {
 
+    }
+
+    private void initialize() {
+        TERenderer ter = new TERenderer();
+        ter.initialize(50, 50);
+        TETile[][] world = new TETile[50][50];
+        for (int x = 0; x < 50; x += 1) {
+            for (int y = 0; y < 50; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
     }
 
 }

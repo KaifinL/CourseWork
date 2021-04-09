d22cca28643d86dacdfb7616b99536e971feccff
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-09 16:51:52 +0800

    Snap: RandomWorldDemo.java, HexWorld.java, BoringWorldDemo.java, RandomUtils.java, Hallway.java

--- proj3/byow/Core/Hallway.java
+++ proj3/byow/Core/Hallway.java
@@ -1,2 +1,23 @@
-package byow.Core;public class Hallway {
+package byow.Core;
+
+import byow.TileEngine.TETile;
+
+// this class is basically to build the hallway by the given parameter
+public class Hallway {
+    // variable here
+    int length;
+    int width;
+    TETile pattern;
+
+    // methods here
+    public Hallway(int length, int width, TETile pattern) {
+        this.length = length;
+        this.width = width;
+        this.pattern = pattern;
+    }
+
+    public void drawHallway() {
+        
+    }
+
 }

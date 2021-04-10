e51b2fa692d31af7fe5902071ba3ae1458349dd9
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-10 08:05:43 +0800

    Snap: RandomWorldDemo.java, HexWorld.java, BoringWorldDemo.java, Hallway.java, Hexagon.java

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,34 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    /**
+     * Fills the given 2D array of tiles with RANDOM tiles.
+     * @param tiles
+     */
+    public static void fillWithNothing(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
+        fillWithNothing(randomTiles);
+
+        ter.renderFrame(randomTiles);
+    }
 }

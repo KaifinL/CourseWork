801bcbd5c09ddcc4439fc8dd1bbc6038078e53dc
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-11 20:56:27 +0800

    Snap: RandomWorldDemo.java, HexWorld.java, Hallway.java, BoringWorldDemo.java, Hexagon.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java

--- proj3/byow/Core/Position.java
+++ proj3/byow/Core/Position.java
@@ -1,2 +1,21 @@
-package byow.Core;public class Position {
+package byow.Core;
+
+//this is actually a focus.
+
+/**
+ * A position (x, y) on the map.
+ */
+public class Position {
+    public int x;
+    public int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void changePos(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
 }

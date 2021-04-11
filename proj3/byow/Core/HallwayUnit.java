055236c9f2eadb25cae99b514599add82cbd62ad
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-11 20:55:05 +0800

    Snap: RandomWorldDemo.java, HexWorld.java, Hallway.java, BoringWorldDemo.java, Hexagon.java, HallwayUnit.java, RoomUnit.java

--- proj3/byow/Core/HallwayUnit.java
+++ proj3/byow/Core/HallwayUnit.java
@@ -1,2 +1,34 @@
-package byow.Core;public class HallwayUnit {
+package byow.Core;
+
+/**
+ * Unit of the Hallway.
+ * Abstractly speaking, hallway is a kind of room.
+ */
+public class HallwayUnit extends RoomUnit {
+
+    public HallwayUnit() {
+        this(randomWidth(), randomLength(), randomDirection());
+    }
+
+    public HallwayUnit(int width, int length, int direction) {
+        super(width, length, direction);
+    }
+
+
+
+    /**
+     * Pick random width between [1, 2].
+     */
+    public static int randomWidth() {
+        return 1 + RANDOM.nextInt(2);
+    }
+
+    /**
+     * Pick random length between [2, 10].
+     */
+    public static int randomLength() {
+        return 2 + RANDOM.nextInt(9);
+    }
+
 }
+

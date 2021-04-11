840aa1e0ad4cf9c97e91da04f6d5e33066589248
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-12 07:36:36 +0800

    Snap: RandomWorldDemo.java, HexWorld.java, Hallway.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java, BoringWorldDemo.java, Hexagon.java

--- proj3/byow/Core/HallwayUnit.java
+++ proj3/byow/Core/HallwayUnit.java
@@ -1,17 +1,29 @@
 package byow.Core;
 
+import java.util.Random;
+
 /**
  * Unit of the Hallway.
  * Abstractly speaking, hallway is a kind of room.
  */
 public class HallwayUnit extends RoomUnit {
+    public static final int scope1 = 2;
+    public static final int scope2 = 9;
 
-    public HallwayUnit() {
-        this(randomWidth(), randomLength(), randomDirection());
+
+    /**
+     * Constructors.
+     */
+    public HallwayUnit(long seed) {
+        this(randomWidth(seed), randomLength(seed), randomDirection(seed), seed);
     }
 
-    public HallwayUnit(int width, int length, int direction) {
-        super(width, length, direction);
+    public HallwayUnit(int direction, long seed) {
+        this(randomWidth(seed), randomLength(seed), direction, seed);
+    }
+
+    public HallwayUnit(int width, int length, int direction, long seed) {
+        super(width, length, direction, seed);
     }
 
 
@@ -19,16 +31,17 @@ public class HallwayUnit extends RoomUnit {
     /**
      * Pick random width between [1, 2].
      */
-    public static int randomWidth() {
-        return 1 + RANDOM.nextInt(2);
+    public static int randomWidth(long seed) {
+        Random r = new Random(seed);
+        return 1 + r.nextInt(scope1);
     }
 
     /**
      * Pick random length between [2, 10].
      */
-    public static int randomLength() {
-        return 2 + RANDOM.nextInt(9);
+    public static int randomLength(long seed) {
+        Random r = new Random(seed);
+        return 2 + r.nextInt(scope2);
     }
 
 }
-

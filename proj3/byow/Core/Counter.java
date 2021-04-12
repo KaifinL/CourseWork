df98708f0e289646361cb66b26158808e8aed164
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-12 15:10:59 +0800

    Snap: HexWorld.java, Hallway.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java, Counter.java

--- proj3/byow/Core/Counter.java
+++ proj3/byow/Core/Counter.java
@@ -1,2 +1,21 @@
-package byow.Core;public class Counter {
+package byow.Core;
+
+public class Counter {
+    // variable here
+    private static int size = 0;
+    private static double complexity = 0;
+    private final static int SQUARE = 400;
+
+    public static double getComplexity() {
+        return complexity;
+    }
+
+    public static int getSize() {
+        return size;
+    }
+
+    public static void changeSize(int change) {
+        size = size + change;
+        complexity = size / SQUARE;
+    }
 }

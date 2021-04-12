0008add7e2d068e54b22d8cba8b29a3238666a28
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-12 19:24:02 +0800

    Snap: HexWorld.java, Hallway.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java, Tileset.java

--- proj3/byow/Core/RandomWorld.java
+++ proj3/byow/Core/RandomWorld.java
@@ -104,7 +104,7 @@ public class RandomWorld {
             Position currExit = exitsQueue.poll();
             newSeed = RANDOM.nextInt((int) newSeed);
             RoomUnit newRoom = randomlyGeneration(newSeed, world, newFocus(currExit));
-            complexity += (double) newRoom.getSize() / 400;
+            complexity += (double) newRoom.getSize() / 100;
         }
     }
 

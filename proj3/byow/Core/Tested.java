ddeae3c2416f3c276247405f1602f86bf73480fc
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-13 16:39:01 +0800

    Snap: HexWorld.java, Hallway.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java, Tileset.java, Tested.java

--- proj3/byow/Core/Tested.java
+++ proj3/byow/Core/Tested.java
@@ -1,2 +1,15 @@
-package byow.Core;public class Test {
+package byow.Core;
+
+import org.junit.Test;
+import java.util.PriorityQueue;
+
+public class Tested {
+    static PriorityQueue<Position> pq = new PriorityQueue();
+
+    @Test
+    public static void test() {
+        Position test1 = new Position(3, 5);
+        pq.add(test1);
+        
+    }
 }

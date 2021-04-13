d9b8842312272f0615b1acf50cf510839681b939
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-13 22:43:09 +0800

    Snap: HexWorld.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java

--- proj3/byow/Core/RandomWorld.java
+++ proj3/byow/Core/RandomWorld.java
@@ -13,7 +13,7 @@ public class RandomWorld {
     private static final int WIDTH = 100;
     private static final int HEIGHT = 40;
 
-    private static final long SEED = 28720;
+    private static final long SEED = 12345;
     private static final Random RANDOM = new Random(SEED);
 
     /**

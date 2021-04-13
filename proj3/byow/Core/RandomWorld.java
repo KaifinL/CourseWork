331f439e2102793d2de21dca630dab0956237287
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-13 21:07:47 +0800

    Snap: HexWorld.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java

--- proj3/byow/Core/RandomWorld.java
+++ proj3/byow/Core/RandomWorld.java
@@ -156,7 +156,7 @@ public class RandomWorld {
         RoomUnit newObject = generateHallway(seed, focus);
         newObject.setFocus(focus);
         if (newObject.checkIndexError(world) || newObject.checkOverlap(world)) {
-            if (tries > 6) {
+            if (tries > 10) {
                 return null;
             }
             focus.changeDirection();

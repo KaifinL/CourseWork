c6cb8ce5c8cf061d32ab8abd1f373391e4bf09b0
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-13 16:44:06 +0800

    Snap: HexWorld.java, Hallway.java, HallwayUnit.java, RoomUnit.java, Position.java, RandomWorld.java, Tileset.java, Tested.java

--- proj3/byow/Core/Tested.java
+++ proj3/byow/Core/Tested.java
@@ -5,10 +5,10 @@ import org.junit.Assert.*;
 import java.util.PriorityQueue;
 
 public class Tested {
-    static PriorityQueue<Position> pq = new PriorityQueue();
 
     @Test
     public void test() {
+        PriorityQueue<Position> pq = new PriorityQueue<>();
         Position test1 = new Position(3, 5);
         pq.add(test1);
         System.out.println(pq.poll());

3f7de88ae6cca3c3b28d8c8640fc813560da0c3e
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-14 17:03:23 +0800

    Snap: Engine.java, RandomWorld.java, RoomUnit.java, InputSource.java, DemoInputSource.java, StringInputDevice.java, RandomInputSource.java, HallwayUnit.java, tests.java

--- proj3/byow/Core/tests.java
+++ proj3/byow/Core/tests.java
@@ -1,5 +1,6 @@
 package byow.Core;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 import byow.TileEngine.TETile;
@@ -9,6 +10,8 @@ import org.junit.Test;
 public class tests {
     @Test
     public void testTwoSameInput() {
-        TETile[][] world1 = Engine.interactWithInputString("")
+        TETile[][] world1 = Engine.interactWithInputString("n5197880843569031643s");
+        TETile[][] world2 = Engine.interactWithInputString("n5197880843569031643s");
+        assertArrayEquals(world1, world2);
     }
 }

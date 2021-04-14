fe711f26febf940ce669d50f286570bf4b7e9719
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-14 16:14:32 +0800

    Snap: Engine.java, InputSource.java, DemoInputSource.java, StringInputDevice.java, RandomInputSource.java, HallwayUnit.java

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -51,6 +51,6 @@ public class Engine {
     }
 
     public static void main(String[] args) {
-        interactWithInputString("n5197880843569031643s");
+        interactWithInputString("n1s");
     }
 }

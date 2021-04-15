55938a96d9c861d854d4e697c7a953958a7e2e6f
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-15 08:21:28 +0800

    Snap: MemoryGame.java, StringInputDevice.java, RandomUtils.java, lab13Tests.java

--- proj3/byow/lab13/MemoryGame.java
+++ proj3/byow/lab13/MemoryGame.java
@@ -57,7 +57,7 @@ public class MemoryGame {
     }
 
     public String generateRandomString(int n) {
-        rand = new Random(789238);
+        rand = new Random(2923);
         String returnStuff = new String();
         for (int i = 0; i < n; i++) {
             int p = rand.nextInt(CHARACTERS.length);

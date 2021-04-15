9404ef9a10b7b48928e9f1da38dffc233016349a
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-15 13:57:48 +0800

    Snap: MemoryGame.java, lab13Tests.java, RandomUtils.java, StringInputDevice.java

--- proj3/byow/lab13/lab13Tests.java
+++ proj3/byow/lab13/lab13Tests.java
@@ -7,7 +7,7 @@ public class lab13Tests {
     @Test
     public void test1() {
         MemoryGame module1 = new MemoryGame(30, 40, 239872);
-        module1.solicitNCharsInput(9);
+        MemoryGame.rounds(4);
     }
 
 

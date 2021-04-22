905e765ec4bbbc57d2c5fb1b813889eb88d875e7
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-22 09:41:36 +0800

    Snap: Engine.java, tests.java, Counter.java, RoomUnit.java, HallwayUnit.java, Avatar.java, SaveLoad.java

--- proj3/byow/Core/tests.java
+++ proj3/byow/Core/tests.java
@@ -51,7 +51,7 @@ public class tests {
 
     @Test
     public void setTestInput() throws IOException {
-        Engine.stringManipulation("N92837SDDDWWWDDD:Q");
+        Engine.stringManipulation("LSDDDWWWDDD:Q");
     }
 
 }

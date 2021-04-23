a0dc3841910a052fd1a4cd810d82767feaa451fc
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-23 12:47:57 +0800

    Snap: Skill.java, Avatar.java

--- proj3/byow/Core/Skill.java
+++ proj3/byow/Core/Skill.java
@@ -1,2 +1,19 @@
-package byow.Core;public class Skill {
+package byow.Core;
+
+/**
+ * this class is about achieving a function that is if the avatar is near a wall,
+ * it can press 'c' to make a new world and further explore it!
+ */
+public class Skill {
+    private Avatar avatar;
+
+
+    public void chisel(Avatar avatar) {
+        this.avatar = avatar;
+    }
+
+    private int getDirection() {
+        Position avatarPos = avatar.
+    }
+
 }

7ac0336d34d74d589928477818a328efd540f369
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-24 15:34:46 +0800

    Snap: RandomWorld.java, tests.java, Engine.java, MemoryGame.java, Skill.java, Avatar.java, SaveLoad.java

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -206,7 +206,7 @@ public class Engine {
     }
 
     public static void main(String[] args) throws IOException {
-        interactWithInputString("N3428174SWWWWW");
+        interactWithInputString("N3428174");
     }
 
 }

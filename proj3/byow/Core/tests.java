477fd2fb9e89017ce40c9824c1fb854f79731ee5
Kaifeng Lin <kefinlin@berkeley.edu>
2021-04-25 18:54:09 +0800

    Snap: RandomWorld.java, Main.java, tests.java, Engine.java, Skill.java, RoomUnit.java, Position.java, RandomInputSource.java, Avatar.java

--- proj3/byow/Core/tests.java
+++ proj3/byow/Core/tests.java
@@ -105,7 +105,7 @@ public class tests {
     @Test
     public void testWithInputString() {
         Engine test = new Engine();
-        test.interactWithInputString("N3428186674WWWAAAAAAAAAAAAA");
+        test.interactWithInputString("N3428186674WWWAAAAAAAAAAAAAC");
     }
 
 }

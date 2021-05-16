b914eee505bc6607f03ddc370a3090734b724de3
Kaifeng Lin <kefinlin@berkeley.edu>
2021-05-16 09:43:47 +0800

    Snap: test.java, Thread1.java

--- ThreadTry/src/Thread1.java
+++ ThreadTry/src/Thread1.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Thread1 {
+public class Thread1 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("KeFin is running");
+    }
+
+    public static void main(String[] args) {
+        Thread1 kefin = new Thread1();
+        Thread trie = new Thread(kefin);
+        trie.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+    }
 }

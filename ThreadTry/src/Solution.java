1a33c9692e369dbe7e785d16362f364f0d9e02ce
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-08 15:40:52 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,6 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 class Solution {
-    public String longestPalindrome(String s) {
-        return s;
+    public static String longestPalindrome(String s) {
+        String output = "";
+        for (int i = 0; i < s.length(); i++) {
+            int k = 0;
+            while (i + k < s.length() - 1) {
+                String subString = s.substring(i, i + k + 1);
+                if (judgement(subString) && subString.length() > output.length()) {
+                    output = subString;
+                }
+                k += 1;
+            }
+        }
+        return output;
     }
 
     public static String helper1(String s, int index) {
@@ -17,10 +31,20 @@ class Solution {
         }
         return output;
     }
+    public boolean judgement(String s) {
+        String reverse = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverse += s.substring(i, i + 1);
+        }
+        if (!reverse.equals(s)) {
+            return false;
+        }
+        return true;
+    }
 
 
     public static void main(String[] args) {
         String s = "babad";
-        System.out.println(helper1(s, 3));
+        System.out.println(longestPalindrome(s));
     }
 }

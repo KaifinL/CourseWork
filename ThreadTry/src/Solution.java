32267314d8c473fe72ae6d5c66611d71547e4455
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-08 15:04:29 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,29 +1,5 @@
-public class Solution {
-    public static int lengthOfLongestSubstring(String s) {
-        int solution = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int length = helperSubstring(s.substring(i));
-            if (length > solution) {
-                solution = length;
-            }
-        }
-        return solution;
-    }
-
-    public static int helperSubstring(String s) {
-        String finalString = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (!finalString.contains(s.substring(i, i + 1))) {
-                finalString += s.substring(i, i + 1);
-            } else {
-                break;
-            }
-        }
-        return finalString.length();
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring(""));
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
     }
 }

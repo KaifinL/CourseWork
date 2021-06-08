6c81f2b804c33f0adc6860c335b41db26dfce90d
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-08 20:39:22 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,43 +1,21 @@
 class Solution {
-    public static String convert(String s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
-        String[] stringSet = new String[numRows];
-        // initialize the stringSet;
-        for (int i = 0; i < numRows; i++) {
-            stringSet[i] = "";
-        }
-        // add the first letter to the substring
-        while (!s.equals("")) {
-            for (int i = 0; i < numRows; i++) {
-                try {
-                    stringSet[i] += s.substring(i, i + 1);
-                } catch (StringIndexOutOfBoundsException e) {
-                    break;
-                }
-            }
-            for (int i = numRows - 2, j = 1; i > 0; i--, j++) {
-                try {
-                    stringSet[i] += s.substring(numRows + j - 1, numRows + j);
-                } catch (StringIndexOutOfBoundsException e) {
-                    break;
-                }
-            }
-            if (s.length() >= 2 * numRows - 1) {
-                s = s.substring(2 * numRows - 2);
-            } else {
-                s = "";
-            }
+    public int reverse(int x) {
+        String variable = String.valueOf(x);
+        String integer = variable;
+        if (variable.contains("-")) {
+            integer = variable.substring(1);
         }
         String output = "";
-        for (int i = 0; i < numRows; i++) {
-            output += stringSet[i];
+        for (int i = integer.length() - 1; i >= 0; i++) {
+            output += integer.substring(i, i + 1);
         }
-        return output;
+        if (variable.contains("-")) {
+            output = "-" + output;
+        }
+        return Integer.getInteger(output);
     }
 
     public static void main(String[] args) {
-        System.out.println(convert("ABCDE", 4));
+        
     }
 }

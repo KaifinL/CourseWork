ef8aaa6b5a0db361abb284dabd215926156f2c84
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-08 15:08:35 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,7 +1,25 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
+    public String longestPalindrome(String s) {
+        return s;
     }
 
-    public static String helper1(String )
+    public static String helper1(String s, int index) {
+        int k = 1;
+        String output = s.substring(0, 1);
+        while (index >= k) {
+            if (s.substring(index - k, index - k + 1).equals(s.substring(index + k, index + k + 1))) {
+                output = s.substring(index - k, index + k + 1);
+                k += 1;
+            }
+            else {
+                break;
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println(helper1(s, 1));
+    }
 }

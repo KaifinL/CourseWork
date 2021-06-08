e3979e4681d3c2e76bbcae4d31b7d83d266bcd11
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-08 19:59:32 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,6 +1,24 @@
 class Solution {
     public String convert(String s, int numRows) {
+        String[] stringSet = new String[numRows];
+        // initialize the stringSet;
+        for (int i = 0; i < numRows; i++) {
+            stringSet[i] = "";
+        }
+        // add the first letter to the substring
+        while (!s.equals("")) {
+            for (int i = 0; i < numRows; i++) {
+                stringSet[i] += s.substring(i, i + 1);
+            }
+            for (int i = numRows - 2, j = 1; i > 0; i--, j++) {
+                stringSet[i] += s.substring(numRows + j, numRows + j + 1);
+            }
+            s = s.substring(2 * numRows - 2);
+        }
         return s;
-        String[][] stringSet = new String[][];
+    }
+
+    public static void main(String[] args) {
+        
     }
 }

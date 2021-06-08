23ece0a386da91785eea1312faac6f1759aa67a8
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-08 12:27:01 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -24,6 +24,6 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
 }

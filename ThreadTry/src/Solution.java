3f8380c0522afd5d1ff3defdc84e3e48405c68c7
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 16:07:38 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,7 +1,16 @@
 public class Solution {
     public static int myAtoi(String s) {
         s = s.replace(" ", "");
+        boolean negative = false;
+        if (s.contains("-")) {
+            negative = true;
+            s = s.replace("-", "");
+        }
         
+
+        if (negative) {
+            s = "-" + s;
+        }
         return Integer.parseInt(s);
     }
 

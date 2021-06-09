18284902ef95d2e750ef5a32edaa4a80d263a88d
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 20:20:47 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,20 +1,26 @@
 class Solution {
     public int romanToInt(String s) {
-
+        if (s.equals("")) {
+            return 0;
+        }
+        return transferring(s.substring(0, 1)) + romanToInt(s.substring(1));
     }
 
+
     public static int transferring(String s) {
-        switch (s) {
-            case "I":
-                return 1;
-            case"V":
-                return 5;
-            case "X":
-                return 10;
-            case "L":
-                return 50;
-            case "C":
-                return 100;
-        }
+        return switch (s) {
+            case "I" -> 1;
+            case "V" -> 5;
+            case "X" -> 10;
+            case "L" -> 50;
+            case "C" -> 100;
+            case "D" -> 500;
+            case "M" -> 1000;
+            default -> 0;
+        };
+    }
+
+    public static void main(String[] args) {
+        
     }
 }

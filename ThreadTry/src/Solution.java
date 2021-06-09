f4c281002cc66b27f84f6b207ffe717d8b4fc13e
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 20:17:14 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -3,6 +3,9 @@ class Solution {
         int smaller = Math.min(height[0], height[1]);
         int output = smaller * 1;
         for (int i = 0; i < height.length; i++) {
+            if (height[i] == 0) {
+                continue;
+            }
             for (int j = output / height[i]; j < height.length - i; j++) {
                 int currSmaller = Math.min(height[i], height[i + j]);
                 int currArea = currSmaller * j;

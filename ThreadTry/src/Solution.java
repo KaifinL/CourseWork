3be3ef4c6a95ae6d2c4cd00fcfffc6030c185f7b
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 15:53:39 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,3 +1,12 @@
 public class Solution {
-    
+    public static int myAtoi(String s) {
+        s.replace(" ", "");
+
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        String s = "    777";
+        System.out.println(myAtoi(s));
+    }
 }

b1eabd0fd2032d3594ea9e202abc6fada21ca188
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-10 07:55:48 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -33,6 +33,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 }

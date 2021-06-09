2206a6704b4c685f9e51c79674fb3b12cd68eaf9
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 19:21:14 +0800

    Snap: test.java, Thread1.java, Solution.java

---
+++ ThreadTry/src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static boolean isPalindrome(int x) {
+        String variable = String.valueOf(x);
+        String output = "";
+        for (int i = variable.length() - 1; i >= 0; i--) {
+            output += variable.charAt(i);
+        }
+        if (output.equals(variable)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(123));
+    }
+}

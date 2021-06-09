92874094aba1b34c6e579d421f4757ea0d3831af
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 19:38:25 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,5 +1,14 @@
 class Solution {
-    public int maxArea(int[] height) {
-        
+    public static int maxArea(int[] height) {
+        int smaller = Math.min(height[0], height[1]);
+        int output = smaller ^ 2;
+        for (int i = 0; i < height.length; i++) {
+            int curr = height[i];
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[] {2, 1}));
     }
 }

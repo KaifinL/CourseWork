be12812716ef9580c6cbb1356ab3533f93f3a864
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-09 15:53:26 +0800

    Snap: test.java, Thread1.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,27 +1,3 @@
-class Solution {
-    public static int reverse(int x) {
-        String variable = String.valueOf(x);
-        String integer = variable;
-        if (variable.contains("-")) {
-            integer = variable.substring(1);
-        }
-        String output = "";
-        for (int i = integer.length() - 1; i >= 0; i--) {
-            output += integer.substring(i, i + 1);
-        }
-        if (variable.contains("-")) {
-            output = "-" + output;
-        }
-        int returnStaff;
-        try {
-            returnStaff = Integer.valueOf(output);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
-        return returnStaff;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(reverse(1534236469));
-    }
+public class Solution {
+    
 }

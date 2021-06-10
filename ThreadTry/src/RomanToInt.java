f26e2d013085f8fc0b7b40bf3e1825932641b368
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-10 08:04:16 +0800

    Snap: test.java, Thread1.java, RomanToInt.java

--- ThreadTry/src/RomanToInt.java
+++ ThreadTry/src/RomanToInt.java
@@ -1,29 +1,39 @@
-class Solution {
-    public static int romanToInt(String s) {
-        if (s.equals("")) {
-            return 0;
+public class RomanToInt {
+    public int romanToInt(String s) {
+        int n = s.length();
+        int roman_int = 0;
+        for(int i=0;i<n;i++)
+        {
+            switch(s.charAt(i))
+            {
+                case 'I' : roman_int = roman_int + 1;break;
+                case 'V' : roman_int = roman_int + 5;break;
+                case 'X' : roman_int = roman_int + 10;break;
+                case 'L' : roman_int = roman_int + 50;break;
+                case 'C' : roman_int = roman_int + 100;break;
+                case 'D' : roman_int = roman_int + 500;break;
+                case 'M' : roman_int = roman_int + 1000;break;
+
+            }
+
+            if(i!=0)
+            {
+                if(((s.charAt(i)=='V')||(s.charAt(i)=='X'))
+                        &&(s.charAt(i-1)=='I'))
+                    roman_int = roman_int-1*2;
+                if(((s.charAt(i)=='L')||(s.charAt(i)=='C'))
+                        &&(s.charAt(i-1)=='X'))
+                    roman_int = roman_int-10*2;
+                if(((s.charAt(i)=='D')||(s.charAt(i)=='M'))
+                        &&(s.charAt(i-1)=='C'))
+                    roman_int = roman_int-100*2;
+            }
         }
-        if (s.length() == 1 || transferring(s.substring(0, 1)) >= transferring(s.substring(1, 2))) {
-            return transferring(s.substring(0, 1)) + romanToInt(s.substring(1));
-        }
-        return transferring(s.substring(1, 2)) - transferring(s.substring(0, 1)) + romanToInt(s.substring(2));
+        return roman_int;
     }
 
-
-    public static int transferring(String s) {
-        return switch (s) {
-            case "I" -> 1;
-            case "V" -> 5;
-            case "X" -> 10;
-            case "L" -> 50;
-            case "C" -> 100;
-            case "D" -> 500;
-            case "M" -> 1000;
-            default -> 0;
-        };
+    public static int myAnswer(String s) {
+        
     }
 
-    public static void main(String[] args) {
-        System.out.println(romanToInt("III"));
-    }
 }

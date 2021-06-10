f53fe23754e2787f5e9e0cfe2c93e86a358601af
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-10 08:53:57 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Solution {
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String shortest = "";
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < shortest.length()) {
+                shortest = strs[i];
+            }
+        }
+
+    }
+
+    public boolean judgement(String strs, String prefix) {
+        
+    }
 }

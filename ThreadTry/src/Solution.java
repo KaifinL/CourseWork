314188bf28a63fd22ae5464a5c6a23a2f1208277
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-10 08:54:29 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,15 +1,33 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {
         String shortest = "";
         for (int i = 0; i < strs.length; i++) {
             if (strs[i].length() < shortest.length()) {
                 shortest = strs[i];
             }
         }
-
+        String output = "";
+        for (int i = 0; i < shortest.length(); i++) {
+            for (int j = 0; j < shortest.length() - i; j++) {
+                String curr = output.substring(i, i + j);
+                if (judgement(strs, curr) && curr.length() > output.length()) {
+                    output = curr;
+                }
+            }
+        }
+        return output;
     }
 
-    public boolean judgement(String strs, String prefix) {
-        
+    public boolean judgement(String[] strs, String prefix) {
+        for (int i = 0; i < strs.length; i++) {
+            if (!strs[i].contains(prefix)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix());
     }
 }

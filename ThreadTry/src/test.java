4a661182a07b46d7d09b038cd7599f52aebe0aef
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-14 15:37:53 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/test.java
+++ ThreadTry/src/test.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
-import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 
 public class test {
 
     @Test
     public void test1() {
-        
+        List tst1 = new ArrayList();
+        tst1.add(1);
+        tst1.add(2);
+        List tst2 = new ArrayList();
+        tst2.add(2);
+        tst2.add(1);
+        assertTrue();
     }
 }

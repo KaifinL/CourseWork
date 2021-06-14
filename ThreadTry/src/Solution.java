2f0af86bb9404e4d76160adaee73964202b4af7c
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-14 15:29:14 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -6,7 +6,15 @@ class Solution {
         List<List<Integer>> finalList = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                
+                for (int k = j + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> newList = new ArrayList<>() ;
+                        newList.add(nums[i]);
+                        newList.add(nums[j]);
+                        newList.add(nums[k]);
+                        finalList.add(new ArrayList<>())
+                    }
+                }
             }
         }
 

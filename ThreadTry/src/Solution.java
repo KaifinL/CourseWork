f90e258149429fa7a9a57b6c14eabb8de61c6919
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-14 15:11:28 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -26,6 +26,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(longestCommonPrefix(new String[] {""}));
+        System.out.println(longestCommonPrefix(new String[] {"dog","racecar","car"}));
     }
 }

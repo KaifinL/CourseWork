bb686689b81786a7e75b828812be8486d661ae6d
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-14 15:44:39 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/test.java
+++ ThreadTry/src/test.java
@@ -1,9 +1,7 @@
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-
 import static org.junit.Assert.*;
 
 
@@ -11,12 +9,6 @@ public class test {
 
     @Test
     public void test1() {
-        List tst1 = new ArrayList();
-        tst1.add(1);
-        tst1.add(2);
-        List tst2 = new ArrayList();
-        tst2.add(2);
-        tst2.add(1);
-        assertTrue(new HashSet<Integer>(tst1).equals(new HashSet<>(tst2)));
+        
     }
 }

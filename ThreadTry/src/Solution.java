c8575c17a8e900d7a36b7c65f9995837b45aff01
Kaifeng Lin <kefinlin@berkeley.edu>
2021-06-14 15:47:13 +0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 import java.util.List;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> finalList = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {

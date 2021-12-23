977f391000885d0bc5299bc35e5892ce161f1f6c
Kaifeng Lin <kefinlin@berkeley.edu>
2021-12-23 04:36:00 -0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java, Quicksort.java, ListNode.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -86,7 +86,15 @@ class Solution {
         }
     }
 
-    
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList return_list = new ArrayList();
+        for (int i = 0; i < numCourses; i++) {
+            return_list.add(i);
+        }
+        for (int list[] : prerequisites) {
+            if (return_list.indexOf(list[1]) > )
+        }
+    }
 
     public static void main(String[] args) {
         ListNode test1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));

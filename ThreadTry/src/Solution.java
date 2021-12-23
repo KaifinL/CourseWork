2b1bc0050d98e66c6b976a24e0f6f89447186dc5
Kaifeng Lin <kefinlin@berkeley.edu>
2021-12-23 04:26:23 -0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java, Quicksort.java, ListNode.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -87,7 +87,7 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        ListNode test1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode test1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         Solution testSo = new Solution();
         testSo.reorderList(test1);
         System.out.println(test1);

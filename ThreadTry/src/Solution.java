d662aa4197e9df6fb53ec9bf72e463d611dab2ca
Kaifeng Lin <kefinlin@berkeley.edu>
2022-01-11 18:31:40 -0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java, ListNode.java, Node.java, Quicksort.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -180,7 +180,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution testy = new Solution();
-        String test = Solution.rotateRight("cs124", 3);
+        String test = Solution.rotateRight("cs124", 5);
         System.out.println(test);
     }
 

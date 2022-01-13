46c2c2f981abb50ed59f9e21efaa4e94afd50483
Kaifeng Lin <kefinlin@berkeley.edu>
2022-01-12 18:48:17 -0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java, ListNode.java, Node.java, Quicksort.java, LastTen.java

--- ThreadTry/src/LastTen.java
+++ ThreadTry/src/LastTen.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LastTen {
+public class LastTen {
+    int lastTen[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int index = 0;
+
+    public void add(int newValue) {
+        this.lastTen[index] = newValue;
+        this.index += 1;
+    }
+
+    public int[] getLastTen() {
+        return this.lastTen;
+    }
 }

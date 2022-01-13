add4f64c0e1fe9e9f52b8e6bfd1f818ef856661b
Kaifeng Lin <kefinlin@berkeley.edu>
2022-01-12 18:52:33 -0800

    Snap: test.java, Thread1.java, RomanToInt.java, Solution.java, ListNode.java, Node.java, Quicksort.java, LastTen.java

--- ThreadTry/src/LastTen.java
+++ ThreadTry/src/LastTen.java
@@ -1,6 +1,6 @@
 public class LastTen {
-    int lastTen[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int index = 0;
+    public int lastTen[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public int index = 0;
 
     public void add(int newValue) {
         this.lastTen[index] = newValue;

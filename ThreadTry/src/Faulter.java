ac90ac61b7cccade43a5a01c7deca8f608323c8e
Kaifeng Lin <kefinlin@berkeley.edu>
2022-01-13 02:44:01 -0800

    Snap: Solution.java, Faulter.java, InsertionSorter.java

---
+++ ThreadTry/src/Faulter.java
@@ -0,0 +1,17 @@
+public class LastTen {
+    public int lastTen[];
+    public int index = 0;
+
+    public LastTen() {
+        this.lastTen = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    }
+
+    public void add(int newValue) {
+        this.lastTen[index] = newValue;
+        this.index = (this.index+1)%10;
+    }
+
+    public int[] getLastTen() {
+        return this.lastTen;
+    }
+}
--- ThreadTry/src/InsertionSorter.java
+++ ThreadTry/src/InsertionSorter.java
@@ -1,2 +1,26 @@
 public class InsertionSorter {
+    public static int sort(Comparable array[]) {
+        int counter = 0;
+        for (int ori_index = 0; ori_index < array.length; ori_index++) {
+            int curr_index = ori_index;
+            for (int prev = ori_index-1; prev>=0; prev--) {
+                if (array[curr_index].compareTo(array[prev]) < 0) {
+                    Comparable middle = array[curr_index];
+                    array[curr_index] = array[prev];
+                    array[prev] = middle;
+                    curr_index--;
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Solution.InsertionSorter test2 = new Solution.InsertionSorter();
+        Comparable testDa[] =  new Comparable[]{5, 1, 6, 9, 2};
+        int result = test2.sort(testDa);
+        System.out.println(result);
+    }
+    
 }

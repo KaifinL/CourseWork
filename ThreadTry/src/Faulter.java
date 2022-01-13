38140063faa74cc37232a9b34fea07ac1668d46b
Kaifeng Lin <kefinlin@berkeley.edu>
2022-01-13 02:47:35 -0800

    Snap: Solution.java, Faulter.java, InsertionSorter.java

--- ThreadTry/src/Faulter.java
+++ ThreadTry/src/Faulter.java
@@ -5,12 +5,16 @@ public class Faulter {
     }
 
     public Exception fault() {
+        if (this.error_message == null) {
+            return new IllegalArgumentException();
+        }
         if (this.error_message == "NullPointer") {
             return new NullPointerException();
         } else if (this.error_message == "ArrayIndexOutOfBounds") {
             return new ArrayIndexOutOfBoundsException();
         } else if (this.error_message == "ClassCast") {
             return new ClassCastException();
-        } else if ()
+        }
+        return new IllegalArgumentException();
     }
 }

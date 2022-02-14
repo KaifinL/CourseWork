61a35a758ef710d605c7b507caafc86c18a8b4eb
Kaifeng Lin <kefinlin@berkeley.edu>
2022-02-13 21:43:10 -0600

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -25,6 +25,22 @@ class Solution {
         return false;
     }
 
+    public int maxSubarraySum(int [][] arr) {
+        assert(arr != null);
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+            if (sum >= max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
         int smallest = arr[1] - arr[0];

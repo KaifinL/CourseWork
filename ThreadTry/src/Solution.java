a6ffedc69a1dbdeea8a28bb1f1475a71ae4a386f
Kaifeng Lin <kefinlin@berkeley.edu>
2022-02-19 22:50:52 -0600

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -3,7 +3,9 @@ import org.junit.Test;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import static java.lang.Math.floorDiv;
@@ -43,6 +45,11 @@ class Solution {
             }
         }
         return max;
+    }
+
+    public int helper(String script) {
+        Map test1 = new HashMap<String, List<String>>();
+        
     }
 
     public List<List<Integer>> minimumAbsDifference(int[] arr) {

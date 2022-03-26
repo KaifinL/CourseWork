8eff1a7c2f1ab7d88d3e8bb61e0a4e137bc12648
Kaifeng Lin <kefinlin@berkeley.edu>
2022-03-26 03:37:22 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -680,9 +680,12 @@ class Node {
             Arrays.sort(nums_array);
             if (nums_array[0] == nums_array[1] && nums_array[3] == nums_array[4]) {
                 System.out.println(40);
-                return;
+            } else {
+                System.out.println(6);
             }
+            return;
         }
+        if ()
 
 
 

22a92656f0288e24bc8fb4612207c26b4bf08556
Kaifeng Lin <kefinlin@berkeley.edu>
2022-03-26 02:15:17 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -573,9 +573,21 @@ class Node {
 
 
     public static void main(String[] args) {
-//
-        Scanner in = new Scanner();
-
+        Scanner in = new Scanner(System.in);
+        int num_data = in.nextInt();
+        // 注意 hasNext 和 hasNextLine 的区别
+        for (int i = 0; i < num_data; i++) {
+            int cards_num = in.nextInt();
+            int nums_array[] = new int [cards_num];
+            for (int j = 0; j < cards_num; j++) {
+                nums_array[j] = in.nextInt();
+            }
+            char char_array[] = new char [cards_num];
+            for (int j = 0; j < cards_num; j++) {
+                char_array[j] = (char) in.nextByte();
+                System.out.println(char_array[j]);
+            }
+        }
     }
 
 

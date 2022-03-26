1f3dcf0082c044c36cf7af89c03c9c2731114c43
Kaifeng Lin <kefinlin@berkeley.edu>
2022-03-26 03:05:45 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -609,19 +609,26 @@ class Node {
         Scanner in = new Scanner(System.in);
         int num_data = in.nextInt();
         // 注意 hasNext 和 hasNextLine 的区别
-        for (int i = 0; i < num_data; i++) {
-            int cards_num = in.nextInt();
-            System.out.println(cards_num);
-            int nums_array[] = new int [cards_num];
-            for (int j = 0; j < cards_num; j++) {
-                int next_int = in.nextInt();
-                nums_array[j] = next_int;
-            }
-//             char char_array[] = new char [cards_num+1];
-//             for (int j = 0; j < cards_num; j++) {
-//                 char_array[j] = (char) in.nextByte();
-//                 System.out.println(char_array[j]);
-//             }
+//        for (int i = 0; i < num_data; i++) {
+//            int cards_num = in.nextInt();
+//            System.out.println(cards_num);
+//            int nums_array[] = new int [cards_num];
+//            for (int j = 0; j < cards_num; j++) {
+//                int next_int = in.nextInt();
+//                nums_array[j] = next_int;
+//            }
+////             char char_array[] = new char [cards_num+1];
+////             for (int j = 0; j < cards_num; j++) {
+////                 char_array[j] = (char) in.nextByte();
+////                 System.out.println(char_array[j]);
+////             }
+//        }
+        int cards_num = in.nextInt();
+        System.out.println(cards_num);
+        int nums_array[] = new int [cards_num];
+        for (int j = 0; j < cards_num; j++) {
+            int next_int = in.nextInt();
+            nums_array[j] = next_int;
         }
     }
 

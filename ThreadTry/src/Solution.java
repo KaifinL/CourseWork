a3eaf374e4c342329ea5dc90fceb47474592375b
Kaifeng Lin <kefinlin@berkeley.edu>
2022-04-23 07:13:37 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -927,11 +927,13 @@ class Node {
     // 注意类名必须为 Main, 不要有任何 package xxx 信息
 
         public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int num = in.nextInt();
-            for (int i =0 ; i < num; i++) {
-                System.out.println(each(in));
-            }
+//            Scanner in = new Scanner(System.in);
+//            int num = in.nextInt();
+//            for (int i =0 ; i < num; i++) {
+//                System.out.println(each(in));
+//            }
+
+            System.out.println(0x);
         }
 
 

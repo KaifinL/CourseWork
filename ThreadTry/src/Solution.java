271f886d58c08568ce5ceab62bab3cda36070dd9
Kaifeng Lin <kefinlin@berkeley.edu>
2022-04-23 06:18:18 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -829,7 +829,26 @@ class Node {
 //    }
 
 
-    
+    public static boolean each(Scanner in) {
+        int sudo[][] = new int[6][6];
+        
+    }
+
+
+
+    // 注意类名必须为 Main, 不要有任何 package xxx 信息
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int num = in.nextInt();
+            // 注意 hasNext 和 hasNextLine 的区别
+            while (in.hasNextInt()) { // 注意 while 处理多个 case
+                int a = in.nextInt();
+                int b = in.nextInt();
+                System.out.println(a + b);
+            }
+        }
+
 
 
 

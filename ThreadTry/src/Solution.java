0035f5f8a918f2c4f9e017786e6db7f02edc5c81
Kaifeng Lin <kefinlin@berkeley.edu>
2022-04-23 07:35:25 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -937,7 +937,7 @@ class Node {
         toPrint += ",";
 
         curr = "0x";
-        curr += para.substring(17, 24);
+        curr += para.substring(17, 23);
         a = Integer.decode(curr);
         toPrint += String.valueOf(a);
 

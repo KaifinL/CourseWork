3dfc57b9e0dca6230579574b166847c3c70fe519
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-06 16:34:00 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1199,7 +1199,7 @@ class Node {
 
     public static void main(String[] args) {
         Solution test = new Solution();
-        System.out.println(test.isMatch("aa", "a"));
+        System.out.println(test.isMatch("aa", "a*"));
     }
 
 

c7664ed701eae5fd1f63ccdb54f3fd57cbb201f9
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-06 15:36:24 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1099,7 +1099,7 @@ class Node {
         boolean [][]LPM = new boolean[s.length()][s.length()];
         for (int i = s.length()-1; i >= 0 ; i--) {
             for (int j = 0; j < s.length(); j++) {
-                if (i == j) {
+                if ((s.charAt(i) == s.charAt(j) && j-i <= 1)) {
                     LPM[i][j] = true;
                 } else if (i > j || s.charAt(i) != s.charAt(j) || !LPM[i+1][j-1]) {
                     LPM[i][j] = false;

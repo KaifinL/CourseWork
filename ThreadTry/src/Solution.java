5394ccac607f6fe0194f79a432e6ed7144d8846a
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-07 17:15:07 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1235,7 +1235,7 @@ class Node {
     public int find2(int[] nums, int target) {
         int left = 0;
         int right = nums.length-1;
-        while (left < right) {
+        while (left <= right) {
             int mid = (left + right)/2;
             if (nums[mid] == target) {
                 return mid;

fcbbf7ccac0a81d075694106966ff4413c758f50
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-07 17:13:23 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1240,7 +1240,7 @@ class Node {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                left = mid;
+                left = mid+1;
             } else {
                 right = mid-1;
             }

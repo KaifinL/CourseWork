65be50fe5a9b8ad70d4cb887e333420e3d2dcb15
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-15 16:42:46 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1279,6 +1279,19 @@ class Node {
         return result;
     }
 
+    * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
     public int deepestLeavesSum(TreeNode root) {
         if (root)
     }

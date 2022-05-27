98eee706c87fc927fe71b5967475373982bf46ed
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-27 09:51:26 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1739,7 +1739,7 @@ class Node {
 
     public static void main(String[] args) {
         Solution test = new Solution();
-        int result = test.numberOfSteps(123);
+        int result = test.findKthNumber(13, 2);
         System.out.println(result);
     }
 

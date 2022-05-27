fe4e486d41b2bb34890a99f7ef6e53481ce308ac
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-27 09:04:12 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1701,11 +1701,7 @@ class Node {
     public int numberOfSteps(int num) {
         int steps = 0;
         while (num != 0) {
-            if (num % 2==0) {
-                num /= 2;
-            } else {
-                num--;
-            }
+            num >> 1;
             steps++;
         }
         return steps;

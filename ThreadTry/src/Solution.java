77c2d9c23efe20acd648de2aad77bc06217dc9f7
Kaifeng Lin <kefinlin@berkeley.edu>
2022-05-29 10:25:01 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1750,9 +1750,17 @@ class Node {
         return true;
     }
 
-//    public int maxProduct(String[] words) {
-//
-//    }
+    public int maxProduct(String[] words) {
+        int max = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i+1; j <words.length; j++) {
+                if (no_common(words[i], words[j])) {
+                    max = Math.max(max, words[i].length()*words[j].length());
+                }
+            }
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
         Solution test = new Solution();

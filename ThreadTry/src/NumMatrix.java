135430c40ed5b6c61197e81746f5748250de806a
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-03 15:05:32 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java, NumMatrix.java

--- ThreadTry/src/NumMatrix.java
+++ ThreadTry/src/NumMatrix.java
@@ -22,7 +22,7 @@ public class NumMatrix {
     }
 
     public int[][] transpose(int[][] matrix) {
-        for (int i = 1; i < matrix.length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = i + 1; j < matrix[0].length; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];

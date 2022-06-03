d612f99546d4c29b1102758a238deb51fc1233ae
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-03 14:28:32 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java, NumMatrix.java

--- ThreadTry/src/NumMatrix.java
+++ ThreadTry/src/NumMatrix.java
@@ -1,19 +1,24 @@
 public class NumMatrix {
 
-    private int[][] matrix;
+    private int[][] sumGrid;
 
     public NumMatrix(int[][] matrix) {
-        this.matrix = matrix;
+        sumGrid = new int[matrix.length+1][matrix[0].length+1];
+        for (int i = 0; i <= matrix.length; i++) {
+            sumGrid[i][0] = 0;
+        }
+        for (int i = 0; i <= matrix[0].length; i++) {
+            sumGrid[0][i] = 0;
+        }
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                sumGrid[i][j] = sumGrid[i-1][j] + sumGrid[i][j-1] + matrix[i-1][j-1] - sumGrid[i-1][j-1];
+            }
+        }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j++) {
-                sum+= matrix[i][j];
-            }
-        }
-        return sum;
+        return 
     }
 
     public static void main(String[] args) {

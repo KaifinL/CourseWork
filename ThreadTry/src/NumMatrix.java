544dc45a1ce4316799d211be4a734540d420a242
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-03 13:41:31 -0500

    Snap: Solution.java, InsertionSorter.java, Faulter.java, NumMatrix.java

--- ThreadTry/src/NumMatrix.java
+++ ThreadTry/src/NumMatrix.java
@@ -1,4 +1,18 @@
 public class NumMatrix {
 
-    
+    private int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        for (int i = row1; i < row2; i++) {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }

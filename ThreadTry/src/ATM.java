eefcb121fb1a01e6822e4ecd9508437ec7a291bf
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-05 12:03:56 -0500

    Snap: Solution.java, NumMatrix.java, InsertionSorter.java, Faulter.java, ATM.java

--- ThreadTry/src/ATM.java
+++ ThreadTry/src/ATM.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ATM {
+class ATM {
+
+    
+
+    public ATM() {
+
+    }
+
+    public void deposit(int[] banknotesCount) {
+
+    }
+
+    public int[] withdraw(int amount) {
+
+    }
 }
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM obj = new ATM();
+ * obj.deposit(banknotesCount);
+ * int[] param_2 = obj.withdraw(amount);
+ */

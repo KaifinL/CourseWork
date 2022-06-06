c1be26cb56352e6e53ac336cc6ac3548613fb506
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-06 08:48:40 -0500

    Snap: Solution.java, NumMatrix.java, InsertionSorter.java, Faulter.java, ATM.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1870,8 +1870,9 @@ class Node {
     public static void main(String[] args) {
         Solution test = new Solution();
         ListNode headA = new ListNode(4, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5)))));
-        ListNode headB = new ListNode()
-        boolean result = test.hasAllCodes("00110110", 2);
+        ListNode headB = new ListNode(5, new ListNode(6, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))))));
+
+        ListNode result = test.getIntersectionNode(headA, headB);
         System.out.println(result);
     }
 

03f6a346c9cd14c262b0814c71cc260b1000809b
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-06 08:47:21 -0500

    Snap: Solution.java, NumMatrix.java, InsertionSorter.java, Faulter.java, ATM.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1876,6 +1876,9 @@ class Node {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         HashSet<ListNode> hash_nodes = new HashSet<>();
         while (headA != null && headB != null) {
+            if (headA == headB) {
+                return headA;
+            }
             if (headA != null) {
                 if (!hash_nodes.contains(headA)) {
                     hash_nodes.add(headA);

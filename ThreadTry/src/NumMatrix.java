8e8fae59735122b33438d0086511305b87a4ee36
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-07 13:43:12 -0500

    Snap: NumMatrix.java, ATM.java, InsertionSorter.java, Faulter.java, Solution.java

--- ThreadTry/src/NumMatrix.java
+++ ThreadTry/src/NumMatrix.java
@@ -65,8 +65,5 @@ public class NumMatrix {
         }
     }
 
-    public static void main(String[] args) {
-        NumMatrix test = new NumMatrix();
-        test.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
-    }
+
 }

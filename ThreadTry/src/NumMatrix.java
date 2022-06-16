793c9d942179524662098af1d2ddc9733f893a2f
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-16 12:57:40 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/NumMatrix.java
+++ ThreadTry/src/NumMatrix.java
@@ -294,5 +294,22 @@ public class NumMatrix {
         return ls[0][1];
     }
 
+    public int binary_search(int []nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+        while (left < right) {
+            mid = (left + right) /2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return (nums[left] == target) ? left : -1;
+    }
+
 
 }

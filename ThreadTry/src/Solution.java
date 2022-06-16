d5ddbde4a26e50cb20b29621eb530ac84070f2a7
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-16 13:10:58 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1876,7 +1876,7 @@ class Node {
 //        ListNode result = test.getIntersectionNode(headA, headB);
 
         NumMatrix test3 = new NumMatrix();
-        int result = test3.binary_search(new int[]{1, 2, 3, 4, 5, 6}, 7);
+        int result = test3.search(new int[]{}, 7);
         System.out.println(result);
     }
 

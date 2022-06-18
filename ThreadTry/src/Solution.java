0be16a4c65dbd5357528109c2c04035cecd35182
Kaifeng Lin <kefinlin@berkeley.edu>
2022-06-18 08:03:17 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1876,7 +1876,7 @@ class Node {
 //        ListNode result = test.getIntersectionNode(headA, headB);
 
         Solution test3 = new Solution();
-        int result = test3.find_last(new int[]{5,7,7,8,8,10}, 5);
+        int result = test3.find_last(new int[]{5,7,7,8,8,10}, 10);
         System.out.println(result);
     }
 

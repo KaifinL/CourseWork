d693bbd8ab23b0606c4aea5dbd36e6e3cdfd3d86
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-05 09:32:48 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/NumMatrix.java
+++ ThreadTry/src/NumMatrix.java
@@ -503,9 +503,9 @@ public class NumMatrix {
                 dir = 'd';
             } else if (dir == 'd' && (i==n-1 || result[i+1][j]!=0)) {
                 dir = 'l';
-            } else if (dir == 'l' && (i == 0 || result[i-1][j] != 0)) {
+            } else if (dir == 'l' && (j == 0 || result[i][j-1] != 0)) {
                 dir = 'u';
-            } else if (dir == 'u' && (j == 0 || result[i][j-1]!= 0)) {
+            } else if (dir == 'u' && (i == 0 || result[i-1][j]!= 0)) {
                 dir = 'r';
             }
 

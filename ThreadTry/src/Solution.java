cb8b2a761626d2e7eb2037714f4de5a94052f806
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-11 12:37:02 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1876,7 +1876,7 @@ class Node {
 //        ListNode result = test.getIntersectionNode(headA, headB);
 
         Solution test3 = new Solution();
-        List result = test3.restoreIpAddresses("25525511135");
+        List result = test3.restoreIpAddresses("0000");
 
         System.out.println(result);
     }

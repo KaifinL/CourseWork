9507c15741b2d1f82f11c725c07bc24615b48cac
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-22 17:20:19 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -2098,6 +2098,28 @@ class Node {
         return head;
     }
 
+    public ListNode sortList(ListNode head) {
+        List<Integer> nodes_val = new ArrayList();
+        ListNode curr = head;
+        while (curr != null) {
+            nodes_val.add(curr.val);
+            curr = curr.next;
+        }
+        int []vals = new int[nodes_val.size()];
+        for (int i = 0; i < nodes_val.size(); i++) {
+            vals[i] = nodes_val.get(i);
+        }
+        Arrays.sort(vals);
+        curr = head;
+        int index = 0;
+        while (curr != null) {
+            head.val = vals[index];
+            index++;
+            curr = curr.next;
+        }
+        return head;
+    }
+
 
 
 

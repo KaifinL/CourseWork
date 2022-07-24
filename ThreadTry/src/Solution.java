051b81c53a213c1c8b4800cff20ed4f146e2f5f9
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-24 14:43:32 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -5,6 +5,28 @@ class Solution {
         return (int) (Math.random() * arr.length);
     }
 
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int left = 1;
+        int right = x/2;
+        while (left < right) {
+            int mid = (left+right)/2;
+            if (mid * mid <= x && (mid+1)*(mid+1) >x) {
+                return mid;
+            } else if (mid * mid < x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        System.out.println(test.mySqrt(444444));
+    }
+
 }
 
 

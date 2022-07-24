961da965363ae9efb05c14034d2e945d11f3c161
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-24 14:46:36 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -14,9 +14,9 @@ class Solution {
             if (mid * mid <= x && (mid+1)*(mid+1) >x) {
                 return mid;
             } else if (mid * mid < x) {
-                left = mid + 1;
-            } else {
                 right = mid;
+            } else {
+                left = mid+1;
             }
         }
         return left;

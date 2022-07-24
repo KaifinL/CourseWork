7ea23524849af03917a882859971bf3f9d3e7980
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-24 12:01:15 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.Stack;
 import java.util.Vector;
 
@@ -2150,7 +2151,54 @@ class Node {
         return 0;
     }
 
-    
+    Set<Float> visited;
+    int result;
+    char [][]grid;
+
+    // the coordinate (x, y) is 1, and it should be added to visited.
+    // now we need to check if any of its neigbor is also island.
+    private void visit(int x, int y) {
+        // check above
+        visited.add(transform(x, y));
+        if (x != 0 && this.grid[x-1][y] == '1' && !visited.contains(transform(x-1, y))) {
+            visit(x-1, y);
+        }
+        // check left
+        if (y != 0 && this.grid[x][y-1] == '1' && !visited.contains(transform(x, y-1))) {
+            visit(x, y-1);
+        }
+        // check below
+        if (x != grid.length-1 && grid[x+1][y] == '1' && !visited.contains(transform(x+1, y))) {
+            visit(x+1, y);
+        }
+        //check right
+        if (y != grid[0].length-1 && grid[x][y+1] == '1' && !visited.contains(transform(x, y+1))) {
+            visit(x, y+1);
+        }
+    }
+
+
+    // convert an coordinate to a floating point;
+    private float transform(int x, int y) {
+        StringBuilder sb = new StringBuilder("0.");
+        sb.append(String.valueOf(y));
+        return (float)x + Float.parseFloat(sb.toString());
+    }
+
+    public int numIslands(char[][] grid) {
+        this.visited = new HashSet();
+        this.grid = grid;
+        this.result=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited.contains(transform(i, j))) {
+                    this.result++;
+                    visit(i, j);
+                }
+            }
+        }
+        return this.result;
+    }
 
 
 

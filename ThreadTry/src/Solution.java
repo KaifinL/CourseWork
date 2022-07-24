2c87569f373c0ecc544a0aa37039b35314042f70
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-24 12:09:50 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, InsertionSorter.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -2182,7 +2182,7 @@ class Node {
     private float transform(int x, int y) {
         StringBuilder sb = new StringBuilder("0.");
         sb.append(String.valueOf(y));
-        return (float)x + Float.parseFloat(sb.toString());
+        return  + Float.parseFloat(sb.toString());
     }
 
     public int numIslands(char[][] grid) {

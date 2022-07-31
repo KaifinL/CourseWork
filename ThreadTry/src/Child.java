2c5dece5d83237e9bf14382e448dd83364ccf913
Kaifeng Lin <kefinlin@berkeley.edu>
2022-07-31 14:53:26 -0500

    Snap: NumMatrix.java, Solution.java, ATM.java, Parent.java, Faulter.java, test.java, Child.java

--- ThreadTry/src/Child.java
+++ ThreadTry/src/Child.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.TreeSet;
 
 public class Child extends Parent {
@@ -14,7 +15,7 @@ public class Child extends Parent {
     }
 
     public static void main(String[] args) {
-        Collection<String> collection = new ArrayList<>();
+        Collection<String> collection = new LinkedList<>();
         collection.add("foo");
         collection.add("bar");
         collection.add("baz");

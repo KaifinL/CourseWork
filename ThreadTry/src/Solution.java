1ccc0affde21b5de5f49f29eb8ae70c6f9139a71
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-09 13:08:01 -0500

    Snap: NumMatrix.java, Solution.java, test.java, ATM.java, Child.java, Parent.java, Faulter.java, Result.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -3,8 +3,16 @@ import java.util.*;
 
 class Solution {
 
-    public static int satisfy(List<List<String>> codeList, List<String> shoppingcard) {
-        
+    public static int next_index(List<String> curr_code, List<String> shoppingCart, int index) {
+        for (; index<shoppingCart.size(); index++) {
+            
+        }
+    }
+
+    // return 1 if the items in shopping cart satisfy the order form codeList without
+    // overlap. Gaps are allowed.
+    public static int satisfy(List<List<String>> codeList, List<String> shoppingCart) {
+
     }
 
     public static void main(String[] args) {

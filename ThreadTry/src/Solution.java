6f2299eb9358a09d1e7bd1648d1cf77336687ec2
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-09 13:01:51 -0500

    Snap: NumMatrix.java, Solution.java, test.java, ATM.java, Child.java, Parent.java, Faulter.java, Result.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -3,7 +3,9 @@ import java.util.*;
 
 class Solution {
 
-    public static int satisfy(List<List<String>> )
+    public static int satisfy(List<List<String>> codeList, List<String> shoppingcard) {
+        
+    }
 
     public static void main(String[] args) {
 

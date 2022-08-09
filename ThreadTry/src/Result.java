d8e69e532cf6c06f1de2b606821d0482de59ce8a
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-09 12:04:14 -0500

    Snap: NumMatrix.java, Solution.java, test.java, ATM.java, Child.java, Parent.java, Faulter.java, Result.java

--- ThreadTry/src/Result.java
+++ ThreadTry/src/Result.java
@@ -1,2 +1,44 @@
-public class Result {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+import java.util.*;
+class Result {
+
+    /*
+     * Complete the 'performOperations' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. 2D_INTEGER_ARRAY operations
+     */
+
+    public static void reverseSubarray(List<Integer> origin, int begin, int end) {
+        for (int i = 0; i <= (end-begin)/2; i++) {
+            int temp = origin.get(begin+i);
+            origin.remove(begin+i);
+            origin.add(begin+i, origin.get(end-i));
+            origin.remove(end-i);
+            origin.add(end-i, origin.get(temp));
+        }
+    }
+
+    public static List<Integer> performOperations(List<Integer> arr, List<List<Integer>> operations) {
+        // Write your code here
+        for (List<Integer> operation:operations) {
+            reverseSubarray(arr, operation.get(0), operation.get(1));
+        }
+        return arr;
+    }
+
 }

e97835b6d422610fe3b1614d710dd2012753528a
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-09 12:17:09 -0500

    Snap: NumMatrix.java, Solution.java, test.java, ATM.java, Child.java, Parent.java, Faulter.java, Result.java

--- ThreadTry/src/Result.java
+++ ThreadTry/src/Result.java
@@ -27,8 +27,7 @@ class Result {
         for (int i = 0; i < (end-begin)/2; i++) {
             int temp = origin.get(begin+i);
             origin.remove(begin+i);
-            int end_element = origin.get(end-i);
-            origin.add(begin+i, end_element);
+            origin.add(begin+i, origin.get(end-i-1));
             origin.remove(end-i);
             origin.add(end-i, origin.get(temp));
         }

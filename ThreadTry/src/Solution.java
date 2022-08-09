66d767462d52ea2b6aa74373451bbf1f49ffdcee
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-09 12:57:28 -0500

    Snap: NumMatrix.java, Solution.java, test.java, ATM.java, Child.java, Parent.java, Faulter.java, Result.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,61 +1,8 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+class Solution {
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+    public static int 
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    public static void main(String[] args) {
 
-        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
-            try {
-                return bufferedReader.readLine().replaceAll("\\s+$", "");
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        })
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        int operationsRows = Integer.parseInt(bufferedReader.readLine().trim());
-        int operationsColumns = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<List<Integer>> operations = new ArrayList<>();
-
-        IntStream.range(0, operationsRows).forEach(i -> {
-            try {
-                operations.add(
-                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                                .map(Integer::parseInt)
-                                .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
-
-        List<Integer> result = Result.performOperations(arr, operations);
-
-        bufferedWriter.write(
-                result.stream()
-                        .map(Object::toString)
-                        .collect(joining("\n"))
-                        + "\n"
-        );
-
-        bufferedReader.close();
-        bufferedWriter.close();
     }
 }

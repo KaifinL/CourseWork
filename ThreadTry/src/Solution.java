1c064746b6ef485770b5be2a7baa8bd9930198e4
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-11 10:41:29 -0500

    Snap: NumMatrix.java, Solution.java, Result.java, test.java, ATM.java, Child.java, Parent.java, Faulter.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -1,26 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> num_index = new HashMap();
-        for (int i = 0; i < nums.length; i++) {
-            if (num_index.getOrDefault(nums[i], -1) == -1) {
-                num_index.put(nums[i], i);
-            } else {
-                int index = num_index.get(nums[i]);
-                if (i - index <= k) {
-                    return true;
-                } else {
-                    num_index.put(nums[i], i);
-                }
-            }
+    public int nthUglyNumber(int n) {
+        if (n <= 6) {
+            return n;
         }
-        return false;
+        List<Boolean> uglys = new ArrayList();
+        // 0 is not ugly.
+        uglys.add(false);
+        // all ugly numbers from 1 to 5.
+        uglys.add(true);
+        uglys.add(true);
+        uglys.add(true);
+        uglys.add(true);
+        uglys.add(true);
+        int counter = 5;
+        int curr = 6;
+        while (counter < n) {
+            if (curr % 2 == 0 && uglys.get(curr / 2)) {
+                uglys.add(true);
+                counter++;
+            } else if (curr % 3 == 0 && uglys.get(curr / 3)) {
+                uglys.add(true);
+                counter++;
+            } else if (curr % 3 == 0 && uglys.get(curr / 5)) {
+                uglys.add(true);
+                counter++;
+            } else {
+                uglys.add(false);
+            }
+            curr++;
+        }
+        return counter;
     }
 
+
     public static void main(String[] args) {
-        int []nums = new int[]{1,2,3,1,2,3};
-        boolean a = containsNearbyDuplicate(nums, 2);
-        System.out.println(a);
+        Solution test = new Solution();
+        System.out.println(test.nthUglyNumber(10));
     }
 }

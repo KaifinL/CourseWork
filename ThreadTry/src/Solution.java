87c039e88382ee2b909793a0aefa7fb99fec3a44
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-17 17:31:13 -0500

    Snap: NumMatrix.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -5,37 +5,45 @@ class Solution {
 
     private int[][] mat;
 
+
+    private void change_status(int x, int y) {
+        int []curr_arr = new int[(int)Math.floor(Math.sqrt(2)*Math.min(mat.length, mat[0].length))];
+        int row = x, col = y, size = 0;
+        while (row < mat.length && col < mat[0].length) {
+            curr_arr[size] = this.mat[row][col];
+            size++;
+            row++;
+            col++;
+        }
+        while (size < curr_arr.length) {
+            curr_arr[size] = Integer.MAX_VALUE;
+            size++;
+        }
+        Arrays.sort(curr_arr);
+        row = x;
+        col = y;
+        size = 0;
+        while (row < mat.length && col < mat[0].length) {
+            mat[row][col] = curr_arr[size];
+            size++;
+            row++;
+            col++;
+        }
+    }
+
     public int[][] diagonalSort(int[][] mat) {
         for (int i = 0; i < mat.length; i++) {
-            int []curr_arr = new int[(int)Math.floor(Math.sqrt(2)*Math.min(mat.length, mat[0].length))];
-            int row = i, col = 0, size = 0;
-            while (row < mat.length && col < mat[0].length) {
-                curr_arr[size] = mat[row][col];
-                size++;
-                row++;
-                col++;
-            }
-            while (size < curr_arr.length) {
-                curr_arr[size] = Integer.MAX_VALUE;
-                size++;
-            }
-            Arrays.sort(curr_arr);
-            row = i;
-            col = 0;
-            size = 0;
-            while (row < mat.length && col < mat[0].length) {
-                mat[row][col] = curr_arr[size];
-                size++;
-                row++;
-                col++;
-            }
+            change_status(i, 0);
         }
-
-
+        for (int i = 1; i < mat[0].length; i++) {
+            change_status(0, i);
+        }
+        return this.mat;
     }
 
     public static void main(String[] args) {
-
+        Solution test = new Solution();
+        int mat[][] = new int[][];
     }
 
 }

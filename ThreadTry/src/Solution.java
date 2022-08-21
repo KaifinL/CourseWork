5f5ea09f2ebdf23c43bc587f8e2f06ec516f2736
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-21 15:27:17 -0500

    Snap: NumMatrix.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -47,9 +47,40 @@ class Solution {
     @para s is the original string given.
     @para target is the string among those 8
      */
-    private int numPalindrome(String s, int target) {
-        int [][]dp = new int[5][s.length()];
-        
+    public static int count(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int lookup[][] = new int[m + 1][n + 1];
+        for (int i = 0; i <= n; ++i)
+            lookup[0][i] = 0;
+        for (int i = 0; i <= m; ++i)
+            lookup[i][0] = 1;
+
+        // Fill lookup[][] in
+        // bottom up manner
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                // If last characters are
+                // same, we have two options -
+                // 1. consider last characters
+                //    of both strings in solution
+                // 2. ignore last character
+                //    of first string
+                if (a.charAt(i - 1) == b.charAt(j - 1))
+                    lookup[i][j] = lookup[i - 1][j - 1] +
+                            lookup[i - 1][j];
+
+                else
+                    // If last character are
+                    // different, ignore last
+                    // character of first string
+                    lookup[i][j] = lookup[i - 1][j];
+            }
+        }
+
+        return lookup[m][n];
     }
 
 

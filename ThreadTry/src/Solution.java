6c0e95bf0c92b49ca6a4da4681dbbbbd65bcedd8
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-21 12:36:53 -0500

    Snap: NumMatrix.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -168,6 +168,22 @@ class Solution {
         return result;
     }
 
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length()<=1)
+            return "";
+        char[] arr = palindrome.toCharArray();
+
+        for(int i=0; i<arr.length/2;i++){
+            if(arr[i] != 'a'){ // if not a then change it to be lexographically smallest
+                arr[i] = 'a';
+                return String.valueOf(arr);
+            }
+        }
+        // if we reach here, there are ONLY 'a' in palindrome string, so we should change the last character to a b
+        arr[arr.length-1] = 'b';
+        return String.valueOf(arr);
+    }
+
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>();
         arr.add(2);

cf825ef38c055471eeffb41d453719fcd877d7fc
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-21 15:35:35 -0500

    Snap: NumMatrix.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -55,27 +55,15 @@ class Solution {
             lookup[0][i] = 0;
         for (int i = 0; i <= m; ++i)
             lookup[i][0] = 1;
-
-        // Fill lookup[][] in
-        // bottom up manner
         for (int i = 1; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
-                // If last characters are
-                // same, we have two options -
-                // 1. consider last characters
-                //    of both strings in solution
-                // 2. ignore last character
-                //    of first string
                 if (a.charAt(i - 1) == b.charAt(j - 1))
                     lookup[i][j] = lookup[i - 1][j - 1] +
                             lookup[i - 1][j];
 
                 else
-                    // If last character are
-                    // different, ignore last
-                    // character of first string
                     lookup[i][j] = lookup[i - 1][j];
             }
         }

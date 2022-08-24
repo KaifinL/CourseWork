b53c0b747d0f55dd7e17b27af38e577595d17d90
Kaifeng Lin <kefinlin@berkeley.edu>
2022-08-23 20:24:40 -0500

    Snap: NumMatrix.java, Solution.java

--- ThreadTry/src/Solution.java
+++ ThreadTry/src/Solution.java
@@ -310,13 +310,13 @@ class Solution {
     }
 
     //football score
-    public List<Integer> football_score(List<Integer> a, List<Integer> b) {
-        a.sort((c, d)->{
-            return c.compareTo(d);
-        });
-        List<Integer> result = new ArrayList<>();
-
-    }
+//    public List<Integer> football_score(List<Integer> a, List<Integer> b) {
+//        a.sort((c, d)->{
+//            return c.compareTo(d);
+//        });
+//        List<Integer> result = new ArrayList<>();
+//
+//    }
 
     private boolean exist_helper(char[][] board, HashSet<String> seen, String word, int index, int row, int col) {
         if (index == word.length()) {
